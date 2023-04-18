4c5e94edf688e093744315763cc676f9ace6ddc9
안태윤 <cfdw011@gmail.com>
2023-04-18 15:27:45 +0900

    [BE] :sparkles: JPA Entity 수정
    JPA Entity 수정
    바퀴 entity를 OHTEntity로 수정
    OHT 호기별 검사 결과 저장할 OHTCheckEntity 추가
    OHT,OHTCheck,WheelCheck,WheelCheckConfirm 연관관계 매핑
    
    Closes #S08P31A301-187
    
    
    Former-commit-id: 082ad0d0466845474034738973822a077e11165e

--- backend/semes/src/main/java/com/ssafy/semes/wheel/model/WheelEntity.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e7885012535eced1507863d8b91599ee3214a28883f063f5c8052097cbfe4969
-size 522
---
+++ backend/semes/src/main/java/com/ssafy/semes/wheelcheckconfirm/model/service/WheelCheckConfirmServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea13f1aa5df213b5bacfb68059cd7f662552d0a3e9c7a029bebe1fad06cdeb79
+size 563
