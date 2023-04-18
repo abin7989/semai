ccc4d6ce386fe5c21cbed4e591cedac1b7cdb7f2
abin7989 <abin7989@gmail.com>
2023-04-18 15:38:14 +0900

    [BE] :sparkles: : ApiResponse 설정 S08P31A301-188
    Former-commit-id: 127f4ffe0f0aef820f7585e2be60ba3f191510c0

---
+++ backend/semes/src/main/java/com/ssafy/semes/transition/controller/TransitionController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ce336ace8271ed9047a9c988ebc844aba1b235748bc8e6d76e0fa1fe6968135e
+size 935
---
+++ backend/semes/src/main/java/com/ssafy/semes/transition/model/service/TransitionService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0593aed2167b93bfd057d88136a67bf4be17b1f069af87fbce3f6e82d1706db4
+size 148
