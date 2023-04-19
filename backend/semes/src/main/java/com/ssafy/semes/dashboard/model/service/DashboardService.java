3afe9282e6332ada54635b71951af49ab9173301
abin7989 <abin7989@gmail.com>
2023-04-19 14:13:41 +0900

    [BE] :sparkles: : 대시보드 구현 S08P31A301-196
    Former-commit-id: c75e819d5dfc62322d6dc2fd8d5466e5aeab6020

---
+++ backend/semes/src/main/java/com/ssafy/semes/dashboard/model/service/DashboardService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0fb027c388c2b854589f4d434e1267fd8de793cd881ef86c7eaac332b1de5983
+size 329
--- backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/repository/WheelCheckRepository.java
+++ backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/repository/WheelCheckRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9732e1623e77549ff397198efa93d9a46d2359468009dd874a991689a7e0a32
-size 326
+oid sha256:e73beff33ed4b63a5e3b9c51334d393e2cc076ee2a4176b8c9415bc940aec4f3
+size 574
