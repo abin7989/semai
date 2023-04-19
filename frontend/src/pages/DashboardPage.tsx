d63430fd8be0228e6a7dc847e66c3f29602c1399
shpark0913 <shpark097754@gmail.com>
2023-04-19 13:38:14 +0900

    :sparkles: [FE] Electron.js 초기 세팅
    - 현재 build 이전이므로 Electron에는 빈 화면 노출
    - 추후 .env에 Browser=none 입력해야 함.
    
    closes #S08P31A301-168
    
    
    Former-commit-id: f225f8822a998163ba0af063819a829ca8aa6dc1

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15d3f6e5be718283a5a8333a831c102ea7fa10cb087391566fd354b31ef34926
-size 162
+oid sha256:d205ce6b295af612408d07072ad6c36ede61e92370b4cceec20610fb3c593e29
+size 294
