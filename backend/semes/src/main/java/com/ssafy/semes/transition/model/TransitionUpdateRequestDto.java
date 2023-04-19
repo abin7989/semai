87721bfd3ad00a870954a0ab35d80ee2a749ac2f
Tune <cfdw011@gmail.com>
2023-04-20 02:08:59 +0900

    :sparkles: [BE] 이미지 관리 API
    볼트 조회 , 삭제, 수정 API method
    
    S08P31A301-200
    
    
    Former-commit-id: 69944036f6fb43d1322bb95796b458f33e73cca8

---
+++ backend/semes/src/main/java/com/ssafy/semes/transition/model/TransitionUpdateRequestDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e33495da09be1775bd09f31145465d6b29c80f586a27af7458bd8c149ca6ca40
+size 336
---
+++ backend/semes/src/main/java/com/ssafy/semes/transition/model/TransitionUpdateRequestVo.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3153d6cced4144c7b6ad9c70169a9bfa94044aec6518e48161b17c18e6775a3a
+size 220
