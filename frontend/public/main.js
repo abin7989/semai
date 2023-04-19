342e1dd726dd2c3fcb505a24aba8355772cf8b3c
shpark0913 <shpark097754@gmail.com>
2023-04-19 13:38:14 +0900

    :sparkles: [FE] Electron.js 초기 세팅
    - 현재 build 이전이므로 Electron에는 빈 화면 노출
    - 추후 .env에 Browser=none 입력해야 함.
    
    closes #S08P31A301-168
    
    
    Former-commit-id: ba224ffe81a45e5e5152c5b063f678611a777e27

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a31c27ebf6107e92c67b8cbe010806ec734e2a36209445aff21095c5a87e8e6
-size 1113
+oid sha256:64903bde57b7b2b127eeda9f3ff0139fc171767a7ae8eb1a03b7580f85d2594a
+size 1137
---
+++ frontend/src/components/globalStyle.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ec5c6d9de684c972ec9e42ed8c71b56b20de9f51b02e8cd43a83e5d524ab79b
+size 1466
