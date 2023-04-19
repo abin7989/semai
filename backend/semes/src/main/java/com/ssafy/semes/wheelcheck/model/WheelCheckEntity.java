8d3b58850bce85ba3b458d57cab0588b88cf018e
abin7989 <abin7989@gmail.com>
2023-04-19 17:09:43 +0900

    [BE] :sparkles: : 대시보드 메인 구현 S08P31A301-196
    Former-commit-id: 1d75460b050b9b7e10aac30531426630ce4cf27b

--- backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/WheelCheckEntity.java
+++ backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/WheelCheckEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:347a603dff8cc3db855bcdabf3137c913e45e9467a0e45f19400530b8919bd64
-size 1565
+oid sha256:032a733aba1e887819c57172f1b098af23517292616b0e4667fe7c712a3faf27
+size 1599
