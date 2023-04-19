52492a5a2ea8f1b2eb42006fd5fa6286cd45c616
gyur1kim <qri1114@gmail.com>
2023-04-19 13:44:51 +0900

    :sparkles: [FE] global style 적용
    - 색상을 :root에 지정
    - data-theme를 이용해 dark모드와 light모드일 때 색상 지정
    - nav바에서 색상 변경되는 것까지 확인 완료
    
    closes S08P31A301-144
    
    
    Former-commit-id: 80b37ee686139190bb5a57be78036c8401a72ecc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-import router from "./Router";
-import {RouterProvider} from "react-router-dom";
-function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
-}
-
-export default App;
+version https://git-lfs.github.com/spec/v1
+oid sha256:654009d57883c23c7e1208a08bfb4b4e1e054a0cd02546970146638abf37bbba
+size 981
