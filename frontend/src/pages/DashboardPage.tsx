a0267927f043c3dce993d5924064b48a6fd7ee86
shpark0913 <shpark097754@gmail.com>
2023-04-20 10:32:26 +0900

    :sparkles: [FE] Dashboard HTML 틀 구성
    - Dashboard를 OHTResult, OHTCheck, OHTTransition, OHTAllResult
      Section으로 구분
    
    closes #S08P31A301-214
    
    
    Former-commit-id: 52f8a78ebab5e807f3db3d39b69d9d9384bb1448

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d41dc431911cd9ab39f0759309bfaa47e49f4225aaed99865f694db43336529
-size 828
+oid sha256:bea06f06aac056581ecace6ffdf5b7775496c1b4a78cf7e2ea191e1baf1937eb
+size 992
