857bd90f46c822b414b94bec438fa7b8a58b1988
gyur1kim <qri1114@gmail.com>
2023-04-20 17:54:40 +0900

    :sparkles: [FE] 레포트용 table 생성중
    - table 태그를 활용해 레포트 생성중
    - 전체적인 틀은 완성했으나 세부적 CSS는 미완성
    
    closes S08P31A301-148
    
    
    Former-commit-id: 2f57ce7b529d4de8f86bee3b242d167f1e9a47b2

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
-function ReportPage() {
-  return (
-    <div>
-      <h1>레포트 페이지</h1>
-    </div>
-  );
-}
-
-export default ReportPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:12ef432c0b7ebd2bd5e622de2ec5e1cc5ce0f7f65922397de99f8f576ce31475
+size 1484
