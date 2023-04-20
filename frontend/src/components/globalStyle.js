c82067077ec11b08111daf237fb3371fb35f9459
gyur1kim <qri1114@gmail.com>
2023-04-20 17:06:53 +0900

    :sparkles: [FE] style 일부 수정
    - global style에서 height 100% 지정
    - main 태그 이용
    
    closes S08P31A301-148
    
    
    Former-commit-id: 8338f7e03bb3d6cb2612b8f0dd5782efa1277917

--- frontend/src/components/globalStyle.js
+++ frontend/src/components/globalStyle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b48de2226bbaa1de485658528044a8a328be1383d4f3c91d36b471f009b4eed1
-size 1977
+oid sha256:19eaf49a43265e5fd7504995126d5287960673a3914afde1920004ab9cd3d24d
+size 1975
--- frontend/src/pages/LayoutPage.tsx
+++ frontend/src/pages/LayoutPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3efc83fabee50cb6bf2c391ae65d12f120c0e0a787012830c584322d3d506aef
-size 266
+oid sha256:1aee9e8a828e86176dd2d990b1eb41b1f582732b02800564f3cfb1b6007d30b4
+size 486
