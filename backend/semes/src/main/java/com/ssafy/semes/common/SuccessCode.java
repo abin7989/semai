bd3634d001645a288abb547085a3c2dff0cd088b
abin7989 <abin7989@gmail.com>
2023-04-21 16:50:50 +0900

    :sparkles: [BE] dashboard main sse->rest 변경 S08P31A301-239
    Former-commit-id: d737ede4b075862ea1bf4e031f74933a74cbacfb

--- backend/semes/src/main/java/com/ssafy/semes/common/SuccessCode.java
+++ backend/semes/src/main/java/com/ssafy/semes/common/SuccessCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cc6e8a980b279e473f7443d0e0d093b1660cbde4f8ce13f3272cf8014f7d13f
-size 1046
+oid sha256:ff9aacd16d07f903026397e49a63805f1d71cfb88c742d32b5391d7a80d7d72c
+size 1111
