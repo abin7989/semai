51abae14c5cb93740f8c7d2d5c662ef99660e35a
gyur1kim <qri1114@gmail.com>
2023-04-21 16:18:50 +0900

    :sparkles: [FE] table component 수정
    - 선택자를 이용해 td태그 선택
    
    closes #S08P31A301-241
    
    
    Former-commit-id: bce3af34e16cc7840092562d19a5961adf352b39

--- frontend/src/components/TableComponents.tsx
+++ frontend/src/components/TableComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59b928758aeb373da46dd70466969ef412cde6f9d4c09b0319dba94ed8ce61d8
-size 1068
+oid sha256:0207fb23374b20f28841bba29dc18e9e6a94b16763a910e488381920d7ee7450
+size 1231
