a26677f24e1b48414fc44cf2db5b420a4cb7aba9
abin7989 <abin7989@gmail.com>
2023-04-21 17:10:19 +0900

    :bug: [BE] 머지 충돌 해결
    Former-commit-id: dbd8dfcc390beb2c1b80d02740b37d3a00493f9c

--- backend/semes/src/main/java/com/ssafy/semes/common/SuccessCode.java
+++ backend/semes/src/main/java/com/ssafy/semes/common/SuccessCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff9aacd16d07f903026397e49a63805f1d71cfb88c742d32b5391d7a80d7d72c
-size 1111
+oid sha256:d2219ae509695472660ec78bffed9e4eeb109158920b086b9fcb661d23fe0875
+size 1220
