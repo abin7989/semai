fa4af4c7d14b963e8c6ddefe953862864a4d4226
abin7989 <abin7989@gmail.com>
2023-04-21 12:13:59 +0900

    :sparkles: [BE] 보고서 상세 검색 구현 및 Slack 템플릿 구현 S08P31A301-239
    Former-commit-id: b3e5c2fc9acabd2857e23d571716742cb6275cb1

--- backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/repository/WheelCheckRepository.java
+++ backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/repository/WheelCheckRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b33c85ac14c211019a6ca974c089268d7666e2ffd662957509c1bddca3a6202
-size 2648
+oid sha256:944ebbc1c9018c37e02ceb029771ae3c6905ccf444718900bdcda603dad66deb
+size 2712
