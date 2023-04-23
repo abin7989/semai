6318ba2ab294923bdcc893af685da4ff8fad15be
gyur1kim <qri1114@gmail.com>
2023-04-24 03:43:49 +0900

    :sparkles: [FE] OHT 조회 기능
    - form 태그들을 활용해 원하는 OHT 검사 정보를 조회할 수 있도록 함
    - CSS는 적용하지 않음
    - 시간 형식은 논의해봐야 함
    
    closes #S08P31A301-241
    
    
    Former-commit-id: be8cbfb9a32396a712a76d9d4cc03027bd785323

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a58adf5800b2f30307989843bd1ea2715dafb75b9499ad6b1a7b26755052ac0
-size 7069
+oid sha256:efc2c7ccb3d7b4ed6b01c746897e436e56524ee2273650763d0c7472a71c59f9
+size 8463
