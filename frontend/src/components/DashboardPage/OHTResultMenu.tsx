b8c7cd0835a0459b36d0ac5ccf0c472abd69ae6d
shpark0913 <shpark097754@gmail.com>
2023-04-24 17:52:38 +0900

    :sparkles: [FE] 대시보드 하단 표 헤더 고정
    - 표 헤더를 고정하고 overflow를 auto로 설정
    
    closes #S08P31A301-334
    
    
    Former-commit-id: b4b9335c3f708f9d14388b9d7de36846e21b8f84

--- frontend/src/components/DashboardPage/OHTResultMenu.tsx
+++ frontend/src/components/DashboardPage/OHTResultMenu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fce12f5875aeb84d50bbbb57b8533668dd729421683adee64132bbdca59d58f3
-size 3828
+oid sha256:ac12c44a2844da18ce4e24c74434b8899a89c946d5824fe6ce93607d8d8df49f
+size 3801
