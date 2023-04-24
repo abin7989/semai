e85c6ca1ec62f3bb8c6be134c53bfaa155815823
김영식 <kimyoungsik7@naver.com>
2023-04-24 16:30:31 +0900

    :sparkles:[BE] Fast API : 분류 모델 통신 코드 구현
    get 요청으로 bolt 이미지의 위치를 쿼리스트링에 실어 보냈을 때 결과값 반환
    
    closes #S08P31A301-388
    
    
    Former-commit-id: d560f7b011cadba447efde7f171b86ce87e3754c

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7419599c833ba613dc12d8fff53c7242bb6efa21179cda474928cfb37c82cf73
-size 2611
+oid sha256:a1e73a04e943286f971c649e40a6ef9955a4d704c49f0beb281cdab7b3919707
+size 2704
