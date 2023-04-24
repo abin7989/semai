fc27fa5dec3dbaa7cb26db764ff14a960e85d47b
abin7989 <abin7989@gmail.com>
2023-04-24 14:53:42 +0900

    :sparkles: [BE] 레포트 jpa 리팩토링 변경 S08P31A301-321
    Former-commit-id: 6e31ab3a49509463dc3c2144fafdffaac6d27e0f

--- backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/repository/WheelCheckRepository.java
+++ backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/repository/WheelCheckRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:944ebbc1c9018c37e02ceb029771ae3c6905ccf444718900bdcda603dad66deb
-size 2712
+oid sha256:7c3c4274f08a4e42e107253f23484748dee9f8920c872253f88b76d10f441663
+size 2750
