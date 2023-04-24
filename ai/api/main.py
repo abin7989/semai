003f735e049081019dbdb23ef457bc5f501683d1
김영식 <kimyoungsik7@naver.com>
2023-04-24 17:08:10 +0900

    :sparkles:[BE] Fast API : 분류 모델 통신 코드 구현
    이미지 분류 요청 시 200, 400, 500 코 드에 따른 return 값 설정 완료
    
    closes #S08P31A301-388
    
    
    Former-commit-id: f0c0071a8e70e30621653e14758119370cdb51ed

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce8df984b314d897a522efee6cd525911f32bd7e7745bc702936ecf1b26b6f41
-size 3058
+oid sha256:1cfe3de685e867fab5babec1dd977179dae931965ee9030f325fb55ca5d71d3f
+size 3723
