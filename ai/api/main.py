a986f5d4523d2791a7afa70fe4f83718371f0bea
김영식 <kimyoungsik7@naver.com>
2023-04-24 16:49:52 +0900

    :sparkles:[BE] Fast API : 휠 이미지 분류 요청시 이미지 경로 설정 완료
    closes #S08P31A301-388
    
    
    Former-commit-id: 387aa8142b682673c75fcc9b619ab64875f686d4

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1e73a04e943286f971c649e40a6ef9955a4d704c49f0beb281cdab7b3919707
-size 2704
+oid sha256:ce8df984b314d897a522efee6cd525911f32bd7e7745bc702936ecf1b26b6f41
+size 3058
