2bb891989e0c06ba2ff501ae986aa3794a0daa98
shpark0913 <shpark097754@gmail.com>
2023-04-25 17:13:02 +0900

    :lipstick: [FE] 로그인 화면 UI 구성
    - 중간 저장을 위한 커밋
    
    closes #S08P31A301-330
    
    
    Former-commit-id: ee487816f505e223ab03991b25becaf8bbf3f67c
    Former-commit-id: c2c4fd0c5186cafad6bfe2c2e946f4984c3e9c06

--- backend/semes/src/main/java/com/ssafy/semes/image/model/repository/ImageRepository.java
+++ backend/semes/src/main/java/com/ssafy/semes/image/model/repository/ImageRepository.java
@@ -1,4 +1,3 @@
-package com.ssafy.semes.image.model.repository;
-
-public interface ImageRepository {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9a203d72779d82054757f72af4da44210ecca1b0cdae8613c08a31248faa0776
+size 86
--- backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/service/WheelCheckService.java
+++ backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/service/WheelCheckService.java
@@ -1,5 +1,3 @@
-package com.ssafy.semes.wheelcheck.model.service;
-
-
-public interface WheelCheckService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:16ff96056ac3968d0228f466de7149c3c6da0d86b1c6b6fed60f6835dd9ab122
+size 91
