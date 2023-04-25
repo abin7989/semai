30c94c71f1fbd77a7daa86bf26b46f20d099cfe2
김영식 <kimyoungsik7@naver.com>
2023-04-25 13:09:28 +0900

    :recycle:[BE] Fast API : api 요청 시 탐지 분류 코드 연결
    코드 세부 정리 마무리
    
    closes #S08P31A301-366
    
    
    Former-commit-id: b5c0a72c5b9c5394f3b768ad7bc244640151972d
    Former-commit-id: 8e719116bb60e17251e48eb1924d189270745e35

--- ai/api/detection/yolo.py
+++ ai/api/detection/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:169356375cea6c9fa4a72aecfc1bcdd6a55bc30d97ebfc55bb9c379b2c30ee44
-size 2146
+oid sha256:9c820cb57b190ba2839eb0ac4cbea12ef25c9777abce17283bbeb7568eb0be19
+size 2399
--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93a4d3be353aba832f935d4b96dcdc845ef45f0686df0941a2695da4fbe69c49
-size 2381
+oid sha256:89a55ec6495a9bb5b7378db1dba4dddd8e422dad1e0b39bb15262a11b0da0b15
+size 2198
