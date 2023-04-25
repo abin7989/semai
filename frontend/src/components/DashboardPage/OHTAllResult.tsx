fc3991be5cc9517df4d0f386702a1a7dc317e176
shpark0913 <shpark097754@gmail.com>
2023-04-25 10:15:08 +0900

    :bug: [FE] 대시보드 하단 표 버그 수정
    - 스크롤 시 헤더의 border가 고정되지 않는 버그 해결
    
    closes #S08P31A301-328
    
    
    Former-commit-id: 5690036f5465f31b86048524823c0408228e9a66

--- frontend/src/components/DashboardPage/OHTAllResult.tsx
+++ frontend/src/components/DashboardPage/OHTAllResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c5163ca5125409eb859ba54be7a86dc6700b6554765aeeb9efcb271ed8ff978
-size 614
+oid sha256:05323b922f481a849b216630cd75fd1af930ea948ae5c2c23cdf45eb37823455
+size 629
