c84ca2915e35c0fde8622e0ae5e49c69cd5b16f7
gyur1kim <qri1114@gmail.com>
2023-04-25 11:04:33 +0900

    :lipstick: [FE] input 태그의 아이콘 변경
    - 기존의 화살표와 캘린더 아이콘 삭제
    - mui 파일을 직접 background-image의 url값으로 넣어서 해결함
      - mui 아이콘을 url로 넣는 방법은 트러블슈팅 작성함
      - 아직 svg파일 색상이 변경되는 것은 구현하지 않음
    
    closes #S08P31A301-
    
    
    Former-commit-id: 058d97f01d92c77969c71503d2e4d7fd4f955deb

--- frontend/src/components/ReportPage/FilterComponents.tsx
+++ frontend/src/components/ReportPage/FilterComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3eb40771f1827d682a63dd4082705d4a38129974ca4df2bad4ddba5ffec40833
-size 639
+oid sha256:21377273e7044afdb43e06440c76dbefdd707c0726b724aa225ab602f10e7201
+size 1247
