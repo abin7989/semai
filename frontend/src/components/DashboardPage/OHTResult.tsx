10987126c613c664f133ceebd8011b43725bdc3f
shpark0913 <shpark097754@gmail.com>
2023-04-25 13:28:10 +0900

    :sparkles: [FE] 대시보드 검사 정보 컴포넌트 구현
    - 스켈레톤 코드 구현
    - 하단의 볼트 현황 제외 완료
    
    closes #S08P31A301-328
    
    
    Former-commit-id: 32d32e9c06f71e7b5f813e0153863475dff64eca

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,26 +1,3 @@
-import Title from "../Title";
-import styled from "styled-components";
-
-const OHTResultContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  grid-row: 1/3;
-  grid-column: 1/2;
-  padding: 10px;
-`;
-
-const OHTResultSec = styled.div`
-  background-color: var(--section-color);
-  flex: 1;
-`;
-
-function OHTResult() {
-  return (
-    <OHTResultContainer>
-      <Title title="OHT 휠 검사 결과" />
-      <OHTResultSec>안녕</OHTResultSec>
-    </OHTResultContainer>
-  );
-}
-
-export default OHTResult;
+version https://git-lfs.github.com/spec/v1
+oid sha256:59791572326e939aa39a94813e65d9b0f6aaad379edb38e882a1df92f23bcb13
+size 3065
