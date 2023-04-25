5f3f0671d251168948cddd5e5b854d4239cabc4a
gyur1kim <qri1114@gmail.com>
2023-04-25 16:05:16 +0900

    :lipstick: [FE] report css
    -
    
    closes #S08P31A301-293
    
    
    Former-commit-id: bd98cb65be2ade6089b756e65dfeeb501a242dc5

--- frontend/src/components/ReportPage/ReportTable.tsx
+++ frontend/src/components/ReportPage/ReportTable.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13c5aafc7c356ab6f5c4ecad2cc50eb73947c63c741a41c6e6729e1f59c32644
-size 6523
+oid sha256:36b5bd8089db6ffd2be2837710861e2ccab03bb4d7273816541c4a2b6adffbb3
+size 6585
