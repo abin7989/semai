71bf7bf36243ac0e043b5aa613d1ccf14702c8bb
김영식 <kimyoungsik7@naver.com>
2023-04-25 15:33:05 +0900

    :sparkles:[BE] Fast API : 분류별 이미지 파일 저장
    분류 별로 이미지 파일을 따로 저장
    
    closes #S08P31A301-366
    
    
    Former-commit-id: fbd69198cb4b6fc5063baaf05e8f8853d3d5a6fc
    Former-commit-id: 05410f5c4398f72424b6b0793465e7fe342fc6d9

--- ai/api/classification/Regnet.py
+++ ai/api/classification/Regnet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42c41e6267596d5bcbd82a7157d9bdca98d575047da92607850a58bb34dc7a29
-size 1794
+oid sha256:f063a12ed3729104538839e839ba7c6d34a8a89fbfb3451676bbd71c462a7980
+size 1831
