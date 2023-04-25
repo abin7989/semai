19ad9bd48bcc3549470bd16c61bcfd0f7f80ea16
김영식 <kimyoungsik7@naver.com>
2023-04-25 12:39:51 +0900

    :recycle:[BE] Fast API : api 요청 시 탐지 분류 코드 연결
    이미지 경로를 받아 탐지하고 자른 사진을 분류모델로 분류
    
    closes #S08P31A301-366
    
    
    Former-commit-id: 5fe6c2be617c519468466b15f92dee52f4889e2d
    Former-commit-id: 20b0673a409eee4fa674aad9d43484b6b92e3ce3

--- ai/api/detection/yolo.py
+++ ai/api/detection/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e9919575073cde5c275af93b7a2223dce6cd24852e78ec104a56499549b56c6
-size 1962
+oid sha256:169356375cea6c9fa4a72aecfc1bcdd6a55bc30d97ebfc55bb9c379b2c30ee44
+size 2146
