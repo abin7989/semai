6d09b24a8637125fd6f1d467aed09b2e393f1bd2
김영식 <kimyoungsik7@naver.com>
2023-04-25 10:50:22 +0900

    :truck:[BE] Fast API : 분류 모델 모듈화
    gpu 사용 코드 추가 수정
    
    closes #S08P31A301-366
    
    
    Former-commit-id: fe91f7fd9d2f08810a3b75260ec31fd087056076

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:918a4e4708bdd506449425b0dcef2b5e1e4afd5c8b89b4412651ad36397f6d02
-size 2414
+oid sha256:7b1805ff491d420f87a1e9deede3600216e920dd47a813fb1b5bf7861dcee289
+size 1721
