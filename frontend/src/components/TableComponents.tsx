e090909a2a0f8e91d0827994268b8c5a04faac97
shpark0913 <shpark097754@gmail.com>
2023-04-25 13:28:10 +0900

    :sparkles: [FE] 대시보드 검사 정보 컴포넌트 구현
    - 스켈레톤 코드 구현
    - 하단의 볼트 현황 제외 완료
    
    closes #S08P31A301-328
    
    
    Former-commit-id: 833f6df3808be3530e7ee8104084cd19ab23486f

--- frontend/src/components/TableComponents.tsx
+++ frontend/src/components/TableComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f7d22453873c17863c8f267d25d1a105ff7c2dba14d6fab8c573e125eea336c
-size 1727
+oid sha256:62eaefcaff2160d9262136cdc33388b8308e86e31286dd15a4d3bfe55d57c756
+size 1649
