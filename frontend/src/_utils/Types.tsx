c4f2072a1115ad1e5a738e72242803601eb00d35
gyur1kim <qri1114@gmail.com>
2023-04-26 14:56:25 +0900

    :lipstick: [FE] detail 모달 창에 Table컴포넌트를 이용하여 표 제작
    - 표 제작
    - detail className을 활용하여 컴포넌트를 재활용할 수 있었다.
    
    closes #S08P31A301-315
    
    
    Former-commit-id: 727599691b717516ca72823987cee5eae5db7975
    Former-commit-id: cfad1650b18a9c14f065b1e567bfa0b28b82e571

--- frontend/src/_utils/Types.tsx
+++ frontend/src/_utils/Types.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dfd77cb693c2bdea5d80e46a9a7d0b2c0d327a007771df2de45c4a34d9017cf
+oid sha256:538d1957ce4cc040d3a6b1b7da7a3d3162ef433fb1b99da1d44331ea539097c2
 size 766
--- frontend/src/components/DetailModal/DetailModal.tsx
+++ frontend/src/components/DetailModal/DetailModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bafed638c0f3fcce948b08ce6f2450daccb1eb98bb8fd3aebe2e2abbad27ec18
-size 718
+oid sha256:9637bd867f7e8ff895317a9c84baedc4593da884d10fded6aceef81a98eceb68
+size 769
