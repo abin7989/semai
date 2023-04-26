05b5828ed3fc9698c0d769ddf229bf7510c0b42b
김영식 <kimyoungsik7@naver.com>
2023-04-26 16:28:50 +0900

    :recycle:[BE] Fast API : crop 코드를 함수로 변경
    crop 후 이미지 저장 결과를 텍스트파일로 저장하는 코드를 함수로 변경
    
    closes #S08P31A301-387
    
    
    Former-commit-id: b5d9da650079df1b17c2c024f00247118fa7f3d2
    Former-commit-id: bdf9240c97b4ef1c93911e3c456afdd58154540f

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cc20013832adcad80f1770d90b24d54fcd074849bb6043cacb7bfa0699ef3e2
-size 2276
+oid sha256:c478e72122ce98cdbb95f3167b275c0c863244b88093462fd5fd572440f8c051
+size 2275
