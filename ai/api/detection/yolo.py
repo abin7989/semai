c5dfcbefdd9abc0d0f86695d6af7b1b3ef3de646
김영식 <kimyoungsik7@naver.com>
2023-04-26 11:36:03 +0900

    :sparkles:[BE] Fast API : 이미지 추론 시간 구현
    이미지 분류시 휠 이미지 디텍팅 후 분류 저장까지 걸리는 시간을 구해 응답으로 넘겨줌
    
    closes #S08P31A301-389
    
    
    Former-commit-id: 81fcc5aa79743ca56ec33443b14e2880cd62a8ff
    Former-commit-id: ef58b79ec9fa827d1b32e12da5f78a59ac711f12

--- ai/api/detection/yolo.py
+++ ai/api/detection/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bc07b8895a20f07e48aa1142682c6ae7d27d5aefe437e87e00eca8bd96c2ea2
-size 2426
+oid sha256:3f6727f1a7acfe87108f2fcda1dee1d063b026a475b2a634237a512790e240f0
+size 2427
