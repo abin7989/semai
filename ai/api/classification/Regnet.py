0f96153becba8e85d0fd3c80f5fd98eea8a509cd
김영식 <kimyoungsik7@naver.com>
2023-04-26 16:51:19 +0900

    :recycle:[BE] Fast API : 이상 표시된 사진 경로까지 추가로 전송
    Fast API 코드 완료
    
    closes #S08P31A301-387
    
    
    Former-commit-id: e0401a4d58cdfc166f948a7662d358fa6b77532c
    Former-commit-id: b510030efe2f0ec668f9557797540fcdfae9c10d

--- ai/api/classification/Regnet.py
+++ ai/api/classification/Regnet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:603f0c6453b585126401803e3bd90759d54aafd4ba9bf49452ecab3a635b1120
-size 4184
+oid sha256:8633d9a49ffe367fc5ea9618c9d1edc42eb6e30ebbf727a97a4e8eb0cd35be06
+size 5137
--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c478e72122ce98cdbb95f3167b275c0c863244b88093462fd5fd572440f8c051
-size 2275
+oid sha256:49fc6e17ab1f8a49fe03ee5310fefd76bc7d49592e966dee6243e3457fda21f9
+size 2301
