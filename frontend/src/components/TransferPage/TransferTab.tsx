78a747123b8137db4b41fc927e966fba24068689
gyur1kim <qri1114@gmail.com>
2023-04-27 13:36:56 +0900

    :sparkles: [FE] 전이학습 페이지 HTML 구조
    - 최상단은 main 태그
    - 전이학습 큰 틀은 section태그
    - 좌측 정상/유실/파단/풀림/모호/학습은 menu 태그와 li태그로 이루어져있음
    
    closes #S08P31A301-320
    
    
    Former-commit-id: 5b6e42af33b1b5e97d64625f7ec8f07821605c1f
    Former-commit-id: b705017a3154e2e230c58b79ee3a1b099e91c76c

---
+++ frontend/src/components/TransferPage/TransferTab.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c7649c063977b58f91db1aaa9dcd8e9730a742fd2f6ccc323f79ced0eab4ae98
+size 1326
--- frontend/src/components/globalStyle.js
+++ frontend/src/components/globalStyle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b70e48927d2481b50146c8cbc177f9532625609073b548b09cbf29ed6ed1c1b1
-size 2559
+oid sha256:0d84da5e12d1eeac678c74c72f56be1c004a0e0504d2d10592a6d221ec07b576
+size 2600
