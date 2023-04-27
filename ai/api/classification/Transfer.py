136cee4b6186879b410626bbed5a533f7635a5b1
김영식 <kimyoungsik7@naver.com>
2023-04-27 16:55:46 +0900

    :sparkles: Fast API : 쿼리 파라미터 추가
    closes #S08P31A301-352
    
    
    Former-commit-id: 8a460222dcdf890bbf327a40136becfe0c3bb1dc
    Former-commit-id: 377602380e5d7a8474d73707b69744b66f4e5c8f

--- ai/api/classification/Transfer.py
+++ ai/api/classification/Transfer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e7e67bbbbc7a9609dda4df86cce4ee6d72f82daf6b25833eacf3eb2c040a191
-size 10688
+oid sha256:13ebd574b9684168d08b6ca2a23c8b7d138228a3ec046fb456ed5d42b78d96ee
+size 10711
--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a385ab8bee44bdaf39e58fdc1c96a9e24537dd73c1613c5dcc32d2cd6e52f29b
-size 3225
+oid sha256:7aca94a967be91fcdfb10e9df960edd34d80e31dcba081ffc3ba0141b6de5870
+size 3279
