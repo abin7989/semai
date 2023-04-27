aeb0fce69cc1d37ae5215409cef74005db82706d
shpark0913 <shpark097754@gmail.com>
2023-04-27 13:34:34 +0900

    :sparkles: [FE] 로그인 기능 구현
    - 경로만 맞게 수정하면 정상적으로 기능
    
    closes #S08P31A301-332
    
    
    Former-commit-id: 09425989697f9225633975c84f1eb9e5996782ac
    Former-commit-id: 69f5ca28698534e1ccb7178d410759e89da00306

--- frontend/src/components/LoginPage/LoginInput.js
+++ frontend/src/components/LoginPage/LoginInput.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d3d383fd8fe46f3e65a2aa2f4e44df254fbc19df30dd7dd11a8b93bc3badf1b
-size 800
+oid sha256:cebf502657babec2b6cfaca6617d310a5dddac2a6e4876c0a868eaad4fa34d78
+size 1708
