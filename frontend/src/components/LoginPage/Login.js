84d52ac55208f707f09c13ef2dd92c7e2733b298
shpark0913 <shpark097754@gmail.com>
2023-04-27 09:49:17 +0900

    :bug: [FE] 버그 수정
    - 모든 input 태그에 배경색 적용되는 버그 해결
      - 로그인 창의 input 태그에 ID 적용하여 해결
    - 로그인 창의 세로 스크롤 해결
      - 하단 div 태그 주석 처리
    
    
    Former-commit-id: 9165ccd1b4cd281f029ab29aed3611565b4bf08d
    Former-commit-id: 54e949314fba9919cc850a21e1f239191ab5d2f0

--- frontend/src/components/LoginPage/Login.js
+++ frontend/src/components/LoginPage/Login.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe191c32af5a9e31a4a010c3bcb410a727361fee03297dd38bbaf0cc2603a8e6
-size 1420
+oid sha256:c0eb16691b79b72c4cac6f79e171e0e5889003fe3324b210a3e1857035276d50
+size 1528
