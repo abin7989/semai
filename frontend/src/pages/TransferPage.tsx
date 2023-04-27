6627ff6995b6a1cd23098df533787d6364d027ec
gyur1kim <qri1114@gmail.com>
2023-04-27 17:08:28 +0900

    :sparkles: [FE] 탭 구현
    - 탭 메뉴와 탭 컨텐츠로 구분
    - 클릭한 탭의 idx를 기억
    
    closes #S08P31A301-320
    
    
    Former-commit-id: 0fbdd77b05ea8a10b95093f587e3858618cd873e
    Former-commit-id: b8a86f10519e0d69f4ee92a461b497588c5f8121

--- frontend/src/pages/TransferPage.tsx
+++ frontend/src/pages/TransferPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4c50382ae47f9ece785ce05cab9927d85ccdb5174e35b9fd8f08800bcb42e4f
-size 2050
+oid sha256:5ef79639e32a80bb4646afc170d9e62f3f4ce135dffc70dab556cdad59f346a4
+size 483
