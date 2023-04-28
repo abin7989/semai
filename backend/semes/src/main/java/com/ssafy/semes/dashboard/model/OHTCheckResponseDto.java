5bbf64bc87e8d041ac391d1d75c42d236fd05776
abin7989 <abin7989@gmail.com>
2023-04-28 16:56:05 +0900

    :sparkles: [BE] 대시보드 데이터 에러 처리 S08P31A301-310
    Former-commit-id: c3b199a67017536d043984e305abf2e7c2b7fbb7
    Former-commit-id: 1d99db225f2d8791f12bfb00b5d84ca04ae74fff

--- backend/semes/src/main/java/com/ssafy/semes/dashboard/model/OHTCheckResponseDto.java
+++ backend/semes/src/main/java/com/ssafy/semes/dashboard/model/OHTCheckResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99f8a43081ac2038e475f7c294413afc5eb7ce38d50ee00dcd9864bc6aa176be
-size 585
+oid sha256:d1c128f3de1ae86f9dbcf54476f2470855a0ee3aff50b636cf4cd547205c6ee9
+size 570
--- backend/semes/src/main/java/com/ssafy/semes/dashboard/model/service/DashboardServiceImpl.java
+++ backend/semes/src/main/java/com/ssafy/semes/dashboard/model/service/DashboardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc9de7c7992288d89fd6845f2caddd8ea4d45c45b4011a0445ed6453341a85af
-size 3515
+oid sha256:cc56110db45601eb9b3d3761b3a5d7035a8edd4b30b4f2d04893af950bfed10d
+size 3900
