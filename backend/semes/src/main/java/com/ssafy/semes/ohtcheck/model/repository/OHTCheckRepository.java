d03660a3b5a11ae54c92bc52bd3d7205f492977a
abin7989 <abin7989@gmail.com>
2023-04-28 16:05:28 +0900

    :sparkles: [BE] 대시보드 N+1 문제 해결 S08P31A301-310
    Former-commit-id: a279b6359d82bfa3b9b122ce67c1bfd56c4ca458
    Former-commit-id: 6b050e4255449957afdb52847586947d0a50c6a1

--- backend/semes/src/main/java/com/ssafy/semes/ohtcheck/model/repository/OHTCheckRepository.java
+++ backend/semes/src/main/java/com/ssafy/semes/ohtcheck/model/repository/OHTCheckRepository.java
@@ -1,9 +1,3 @@
-package com.ssafy.semes.ohtcheck.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.ssafy.semes.ohtcheck.model.OHTCheckEntity;
-@Repository
-public interface OHTCheckRepository extends JpaRepository<OHTCheckEntity,Long> {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ee9566d18fc7acb3ffac684cc50eb01ac17bfa04f539b7b8ecb8e71dc476a8a
+size 528
