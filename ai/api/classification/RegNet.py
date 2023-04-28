adc457f56c669996775306c10bae81beab452971
김영식 <kimyoungsik7@naver.com>
2023-04-28 10:28:49 +0900

    :sparkles: Fast API : 전이학습 후 최적 모델 저장 구현
    closes #S08P31A301-377
    
    
    Former-commit-id: c5691041beef66e9c0962bb71c294fb75e828897
    Former-commit-id: dbd938173a15e274815f08b47c5711bdf7befd50

--- ai/api/classification/RegNet.py
+++ ai/api/classification/RegNet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8633d9a49ffe367fc5ea9618c9d1edc42eb6e30ebbf727a97a4e8eb0cd35be06
-size 5137
+oid sha256:74d21e9e0e062b448b43f063a66f073c8886bf4bf83d0acd39bab0048d4b494a
+size 5177
--- ai/api/detection/yolo.py
+++ ai/api/detection/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dff3e8bca6b5808abdc2f4357258c6dcba42c278c5382bd0f288096fe28eb9a9
-size 2753
+oid sha256:3e13171a21a53d86d09007d571c6297e9595a51e1185c47b8dd7c6477ddc7c1e
+size 2777
