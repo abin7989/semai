e97dc1c8f4ec51c417b5a9dc4155c1b619f7dec8
gyur1kim <qri1114@gmail.com>
2023-04-28 11:31:58 +0900

    :lipstick: [FE] 전이학습 CSS 세부사항
    - 선택한 이미지의 갯수 알려주기
    
    closes #S08P31A301-322
    
    
    Former-commit-id: adb957fb4fc7003b6e99368e6388bc1e351479d6
    Former-commit-id: d931f0ed5f3049b2280780bdccff27ba2a865d80

--- frontend/src/components/TransferPage/TransferTab.tsx
+++ frontend/src/components/TransferPage/TransferTab.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fbf2707c2b7543a82acebd5da4318650937793851002585754ff8e3baf4e1c1
-size 8919
+oid sha256:9d6706c880ab979e3b547ca9b6c6456029b584f328faa310cb66505cceeda70c
+size 10142
