d2a07ce6b18ff7d0da6da0a264106636c77d6a7d
gyur1kim <qri1114@gmail.com>
2023-04-28 18:02:52 +0900

    :sparkles: [FE] redux에 token과 userName 저장
    - useDispatch를 이용해 로그인 시 값을 저장함
    - axios에서 token값을 사용하기 위해 getState()를 통해 store에 접근
      - 그런데 자꾸 store의 init 값을 가져온다........ 아!!!!!!!!
    
    closes #S08P31A301-324
    
    
    Former-commit-id: 4325aaef0b345d5fc4e9ed540969ff6ea9229d53
    Former-commit-id: 70a63b5e5c9b8012fb6d2e49aceb52542755bbe9

--- frontend/src/_store/store.ts
+++ frontend/src/_store/store.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e0ef58b35f84fe61f01bcb4dd8882f4ae4f9b7780c5de3acd8230c8ffbfc0e2
-size 926
+oid sha256:006d88cff6d4fec8e5779fdbf8864060e314e1b2a3724e6e78780275c2d7e634
+size 913
