39851bcdf930545d9d3e9d04dd83bfd8c8af5fed
abin7989 <abin7989@gmail.com>
2023-04-28 14:38:15 +0900

    :sparkles: [BE] Fetch 어노테이션 삭제 S08P31A301-310
    Former-commit-id: 6bdaafa7ba2616ff5447e4c40af958c761c5e187
    Former-commit-id: 6dc0cc1ece95fd938980b318074d8143b5c19618

--- backend/semes/src/main/java/com/ssafy/semes/oht/model/OHTEntity.java
+++ backend/semes/src/main/java/com/ssafy/semes/oht/model/OHTEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fabdb5c12b5b950f42c0a8e7bd5df11f950c1dbe607a7d200b9614ec1ac00b89
-size 1339
+oid sha256:d0437a079e72c8e6ecbe8109396bfe635e158d7843538173fd2d0620fb3eb550
+size 1309
