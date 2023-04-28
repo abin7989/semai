13f3557f8afead6cbafc97081f396ce1ed689c33
gyur1kim <qri1114@gmail.com>
2023-04-28 10:54:29 +0900

    :lipstick: [FE] 전이학습 페이지 기본 구조 완료
    - grid를 div태그로 한 번 더 감싸서 스크롤 문제 해결
    - 버튼과 전체선택 배치
    
    closes #S08P31A301-322
    
    
    Former-commit-id: fad0282566d6acb6fa9258ab748ff78c6a92f03b
    Former-commit-id: 3ba7e9f9fe60255b28f4562d350806f19a98251d

--- frontend/src/components/TransferPage/TransferTab.tsx
+++ frontend/src/components/TransferPage/TransferTab.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecfa7856601de6ccdc05b01da4545f853917b82bb52823203a6628dbe0483366
-size 8487
+oid sha256:e3b24a8ff5f5fac94b7a93696988f9fef643ebaecdc7685bbcf0da44c3aad55f
+size 8694
