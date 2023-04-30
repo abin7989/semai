fe2b6504ddb90d10744830a445c133bb63ae1593
gyur1kim <qri1114@gmail.com>
2023-05-01 01:12:57 +0900

    :sparkles: [FE] purge 적용
    Former-commit-id: c87d4d5938c4408e7aba6cc05089e6a4cb9cd031
    Former-commit-id: a4ea373197403cb0af6e06d31ce6d2489b2fd8c3

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,13 +1,3 @@
-import { Link } from "react-router-dom";
-import Login from "../components/LoginPage/Login";
-import React from "react";
-
-function LoginPage() {
-  return (
-    <div>
-      <Login></Login>
-      {/* <Link to="/">홈으로</Link> */}
-    </div>
-  );
-}
-export default LoginPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5d867f97c2026d8cc9297ea7b5fe450e5edd09a8050a1035fd2cc33aac01a1a
+size 189
