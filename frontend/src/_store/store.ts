a36b653ecb4111be6483a6b4b1fe08f97e9a9e35
gyur1kim <qri1114@gmail.com>
2023-04-30 23:52:31 +0900

    :bug: [FE] persist:root가 삭제됐을 경우를 대비
    - 해당 key가 local storage에서 삭제되면 기존의 코드로는 토큰을 조회할 수 없음
      - JSON.parse는 null값이 들어올 수 없음
      - 따라서 항상 문자열 값이 존재할 때만 실행해야 함
    
    closes #S08P31A301-324
    
    
    Former-commit-id: c2e91a790189f06f3b4a0f28f10998564e88016b
    Former-commit-id: ce1458c0e295de2f5a901f3a54c218787a8a45a6

--- frontend/src/_store/store.ts
+++ frontend/src/_store/store.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0bb29e460ffa0bccd31740a7592ae33e7eaacdf0ded3de5542e449fc777a1db
-size 948
+oid sha256:46d5a373ba864b88e96b604771c807583f6d5488f9c49ca9bae829013dae8db0
+size 945
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5ede530a79d13858fec6dc5683d590abbac11026bbee8ff0f957116d63a2442
-size 3760
+oid sha256:2a29ae70d00a2d858733130da432a179ea413193b765582e53b9100feafb273c
+size 3767
