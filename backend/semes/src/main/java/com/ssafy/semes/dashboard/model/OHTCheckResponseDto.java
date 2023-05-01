d93d0a806eedc188893aa4126b9a473cf4e09ef6
김민식 <abin7989@gmail.com>
2023-05-01 10:22:15 +0900

    :sparkles: [BE] JAP 수정 및 대시보드 에러 수정 S08P31A301-402
    Former-commit-id: 0090d6a6ad72cddd4d5de36c8ca89499594874b6
    Former-commit-id: f9150323962af86ada1628c322fee25bc43f2e6d

--- backend/semes/src/main/java/com/ssafy/semes/dashboard/model/OHTCheckResponseDto.java
+++ backend/semes/src/main/java/com/ssafy/semes/dashboard/model/OHTCheckResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1c128f3de1ae86f9dbcf54476f2470855a0ee3aff50b636cf4cd547205c6ee9
-size 570
+oid sha256:bf16fcddbadfad8c2ef338704141e4b3212ed7903b20198fa2736a779b7bdc75
+size 531
--- backend/semes/src/main/java/com/ssafy/semes/ohtcheck/model/repository/OHTCheckRepository.java
+++ backend/semes/src/main/java/com/ssafy/semes/ohtcheck/model/repository/OHTCheckRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ee9566d18fc7acb3ffac684cc50eb01ac17bfa04f539b7b8ecb8e71dc476a8a
-size 528
+oid sha256:4998a4e1c340be81165536643344c2f93532538dd436a6f85ce210a7e936ca25
+size 501
