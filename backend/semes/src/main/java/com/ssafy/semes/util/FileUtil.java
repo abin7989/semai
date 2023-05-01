0e798282f687b82e9d90f7572473734916552397
안태윤 <cfdw011@gmail.com>
2023-05-01 16:20:11 +0900

    :bug: : [BE] 이미지 데이터 폴더 구조 초기화 코드 수정
    이미지 초기화 시 OS에 따라 ROOT Directory 초기화 분기
    Linux -> /dataset , Windows -> parent directory of parent directory/dataset
    Jenkins 설정 수정 필요
    
    Closes #feature-S08P31A301-400
    
    
    Former-commit-id: d137ae31793bf7f46f48bb171e29cccf48b74679
    Former-commit-id: a4734e439bd52d12b965804b8ca4a0ce7b02fa71

--- backend/semes/src/main/java/com/ssafy/semes/util/FileUtil.java
+++ backend/semes/src/main/java/com/ssafy/semes/util/FileUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0bf73a0bfea247dca9868c59cc05d1c40bf8244bace6c8ee57cc324d9b70d23
-size 2439
+oid sha256:c50073193c9bdc14004688228caff46ef0a409ec6d3321b1090f4222427c932c
+size 2517
