3515bb4905cfff88bb9a1462320d39f1bd03e40d
gyur1kim <qri1114@gmail.com>
2023-05-01 16:15:56 +0900

    :sparkles: [FE] 레포트 data 표에 나타내기
    -
    
    closes #S08P31A301-399
    
    
    Former-commit-id: bfcaa4e41e19ce91abc79ebc359a6bf50ca9d4ad
    Former-commit-id: a0255d52cf789d07d38ed099717af9770c7fd476

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:364bb4af34e7eedb64dc8a7bda7404f5057bf0fff11a2649359042bf929c3be5
-size 1337
+oid sha256:22de73634b92e8571f34dcd3302e8199a993552002fa3135a076f8555d36e518
+size 1432
