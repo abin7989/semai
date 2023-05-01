eb14fab9f27b60088fa79c75afb4aa8a3e65759e
shpark0913 <shpark097754@gmail.com>
2023-05-01 13:19:33 +0900

    :sparkles: [FE] OHT 휠 검사 현황 마무리
    - 최신 검사가 상단으로 위치하게 수정
    - 전체, 에러 클릭 시 해당 항목들만 조회 가능
    
    closes #S08P31A301-434
    
    
    Former-commit-id: 149363caac72d88cb341f26285c21e3a96357a7d
    Former-commit-id: e510f111ba42af89796041cc1a6bf03dfa7cdb9e

--- frontend/src/components/DashboardPage/OHTAllResult.tsx
+++ frontend/src/components/DashboardPage/OHTAllResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05323b922f481a849b216630cd75fd1af930ea948ae5c2c23cdf45eb37823455
-size 629
+oid sha256:63163613fb7499d8bc4628bd12d2e75bc26373537e9ed8589a04aa6c6c04d18f
+size 2087
