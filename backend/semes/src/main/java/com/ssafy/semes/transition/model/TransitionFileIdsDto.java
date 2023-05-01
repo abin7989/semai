fe4cbb750dfea1496955911d0c9e195eb4fc0b01
김영식 <kimyoungsik7@naver.com>
2023-05-01 10:35:07 +0900

    :recycle: Fast API : 전이학습 코드 모듈화
    closes #S08P31A301-470
    
    
    Former-commit-id: 1d2039abbf1da14c9b39792f0a38513bef7d64f6
    Former-commit-id: 3aced198a4272181f328d925336c371e39970467

--- backend/semes/src/main/java/com/ssafy/semes/transition/model/TransitionFileIdsDto.java
+++ backend/semes/src/main/java/com/ssafy/semes/transition/model/TransitionFileIdsDto.java
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:77c3b137f8b7bc5f16883cd7d755447bd52f80a86285fad566ff911d95c1d5e9
-size 167
+package com.ssafy.semes.transition.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TransitionFileIdsDto {
+	private Long[] fileIds;
+}
