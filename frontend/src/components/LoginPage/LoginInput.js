123380d2da2d09b7139e71da64e0b2c92707b378
shpark0913 <shpark097754@gmail.com>
2023-05-01 14:15:24 +0900

    :bug: [FE] 콘솔창 에러 수정
    - autoFocus="true"를 autoFocus={true} 로 변경
    
    closes #S08P31A301-408
    
    
    Former-commit-id: 939c726133af05c9b0f177ee09d15d821fd549ce
    Former-commit-id: 3cb15335fc8ca09cd35f29a28a85f528855a7f61

--- frontend/src/components/LoginPage/LoginInput.js
+++ frontend/src/components/LoginPage/LoginInput.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26672f30f089749469eaec5447f805b6ac1091b884140066ce7590ffa97b0ded
+oid sha256:5e5a89703ac4cc01073280f8c3e0d7933c6a6c21fb51457febd3eec7a1caf146
 size 2564
