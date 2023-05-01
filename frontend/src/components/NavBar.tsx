7e18e146b68dabe37e65a3c245cf10202e59f92d
gyur1kim <qri1114@gmail.com>
2023-05-02 01:37:46 +0900

    :bug: [FE] input에서 params가 빠질 때 생기는 에러 해결
    - Form tag는 무조건 action 속성으로 연결되는 줄 알았다.
    - Form tag 중에 method가 get이면 action이 아니라 loader 속성에 연결된 함수를 실행한다.
    
    closes #S08P31A301-324
    
    
    Former-commit-id: 9282f587b857ee07ef187d2de2c09762cf66ad70
    Former-commit-id: 0ccac9ed102c33af276c40505d3bcf843a228637

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb9f7414ec8ef598949f347f19e15ca80728b7cf151642b56948219760b1cee6
+oid sha256:bd7ac390d7daf8f4a137cee7c4cbbf62c12034b6e210f9fd81b28ce1fd9b3c5e
 size 3570
--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2439ed1430633172e3851225b5a56fed9aedf41157d5ceb009c27690a52795c8
-size 5820
+oid sha256:8400d45b8f3b1f7e6e2657bbe8360583a20a55908fc553abe4bc66a6f1c3c868
+size 6533
