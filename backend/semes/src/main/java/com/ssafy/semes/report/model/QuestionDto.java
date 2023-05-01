58ad184c047a18cc2d1dac88bc18e43d9e6a0a69
abin7989 <abin7989@gmail.com>
2023-05-01 10:54:31 +0900

    :sparkles: [BE] 레포트 오류 페이지 및 기간 구현 S08P31A301-429
    Former-commit-id: f9e38a2fbcf024accf38ca78e84ca8c86046ce21
    Former-commit-id: 5e1710c08d2e91937153969d750e866b5df7ce8a

--- backend/semes/src/main/java/com/ssafy/semes/report/model/QuestionDto.java
+++ backend/semes/src/main/java/com/ssafy/semes/report/model/QuestionDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c12d02be54f13ab688556ea53dee136340d18cf18352e84e702bfba3087d759
-size 434
+oid sha256:3e9a9b8bce6e16698e7e6165ea0fe4029a69d9e6b716525477f1e02bb69ef625
+size 494
