4ea2288aeb40d3bac42c15d56f13bc84053cde20
shpark0913 <shpark097754@gmail.com>
2023-05-01 10:36:54 +0900

    :sparkles: [FE] OHT 검사가 NG인 경우 배경색 지정
    - 삼항 연산자로 배경색 지정
    
    closes #S08P31A301-434
    
    
    Former-commit-id: 5f0a2030fd4b695468c194325ac19edfff694cea
    Former-commit-id: 92a1e9be3ef8b09345fc3449d6915de7298e1014

--- frontend/src/components/DashboardPage/OHTResultMenu.jsx
+++ frontend/src/components/DashboardPage/OHTResultMenu.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48e173cab60c7f5b68027fcd5947953feb5a342aa29607a4fb675eda394e9d6b
-size 2162
+oid sha256:69fe5d763a0ddb71179d6cfad80076600d3145b83b3afff43616df8b8287be3f
+size 2267
