982103d8b7310222733fd110fee0da4b3c3fa6c4
shpark0913 <shpark097754@gmail.com>
2023-05-01 09:10:48 +0900

    :art: [FE] 코드 형식 변경
    - 반복되는 코드 반복문으로 수정
    - 반복문 key 설정
    
    
    Former-commit-id: a9423e5d925c9311a3afc5e22d0acf1d0cac23e2
    Former-commit-id: 1bef9af55e239882876fb6b22854e35a96070611

--- frontend/src/components/DashboardPage/OHTResultMenu.jsx
+++ frontend/src/components/DashboardPage/OHTResultMenu.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc3f62eaaa72ae5adb1ecbf372ce703df82f37b749d15f697369601af62e666d
-size 2116
+oid sha256:48e173cab60c7f5b68027fcd5947953feb5a342aa29607a4fb675eda394e9d6b
+size 2162
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,13 +1,3 @@
-import { Link } from "react-router-dom";
-import Login from "../components/LoginPage/Login";
-import React from "react";
-
-function LoginPage() {
-  return (
-    <div>
-      <Login></Login>
-      {/* <Link to="/">홈으로</Link> */}
-    </div>
-  );
-}
-export default LoginPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:7614cfbe6980121bc6e4100a2f4817095b475e1c5c1b9bda16bade23623bc048
+size 274
