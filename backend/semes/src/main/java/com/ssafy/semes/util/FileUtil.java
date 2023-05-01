c7ed6b1fa65f9713ce2d129163713104e5949cba
안태윤 <cfdw011@gmail.com>
2023-05-01 15:43:10 +0900

    :feature: [BE] 전이학습 이미지 폴더 초기화 코드
    폴더 초기화시 OS 별로 분기
    
    Closes #S08P31A301-400
    
    
    Former-commit-id: 229be982cbae8397e3da4d6fec55525dc18113f7
    Former-commit-id: f98b7f0ea2c49035b04b5bc09ad9b41f6f5ec964

--- backend/semes/src/main/java/com/ssafy/semes/util/FileUtil.java
+++ backend/semes/src/main/java/com/ssafy/semes/util/FileUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d68765994debb19e35d73e00324ebe216f740cb1aebb2cf792b1bc5e44748b66
-size 2377
+oid sha256:c0bf73a0bfea247dca9868c59cc05d1c40bf8244bace6c8ee57cc324d9b70d23
+size 2439
