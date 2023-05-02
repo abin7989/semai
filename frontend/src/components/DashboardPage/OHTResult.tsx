653dd8ddfc1d12241923977b68b02a2b311de130
shpark0913 <shpark097754@gmail.com>
2023-05-02 16:26:02 +0900

    :sparkles: [FE] OHT 휠 검사 결과 새로고침 기능 구현
    closes #S08P31A301-440
    
    
    Former-commit-id: 4cbfb22c7fd6f6c5029f929a8b5cd0b6b00c46ba
    Former-commit-id: cc2166353e699a508a16582d5e092bd0cc29ca87

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:021753913f981a6a6eddbc47c56a7448eb9a4cba6f1591ddc8cd9d931db44e0b
-size 4915
+oid sha256:db7012724c48a1892c45e1088a413c3fff67163dc3a8944f151803d5a7aad58a
+size 5399
