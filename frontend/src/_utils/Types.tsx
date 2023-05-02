748db1bdebf89d316feefa1730b9ca2f8f7dd661
gyur1kim <qri1114@gmail.com>
2023-05-02 11:28:45 +0900

    :sparkles: [FE] 모달창 detail
    - props로 값을 넘긴다.
    
    closes #S08P31A301-399
    
    
    Former-commit-id: 2662199f60a51a7878f28a2d5b758542a7862403
    Former-commit-id: 66fc7ed20b547bef004e2a612a32558afb71aa48

--- frontend/src/_utils/Types.tsx
+++ frontend/src/_utils/Types.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e5e7ff6d4194d72d472b57910d8a988a04468fee5dab01118fc9ae6650f4b48
-size 1317
+oid sha256:dfc91f34f6a506cc27fa7c2cec8fe9460a631738c6b94e156be6d00f804a58ce
+size 1279
--- frontend/src/components/ReportPage/ReportTable.tsx
+++ frontend/src/components/ReportPage/ReportTable.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1af76406634c299d79a3e9144b87e585edf00d19574f10d0e1f78f123a6b98d
-size 2457
+oid sha256:bcd267b8693829a81ec9f120da69634151710ef3275eab5e32e04d433f551b4c
+size 2156
