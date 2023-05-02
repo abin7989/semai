31c8394411aebc9f9f0b167b6efebf0a46f41404
shpark0913 <shpark097754@gmail.com>
2023-05-02 16:26:02 +0900

    :sparkles: [FE] OHT 휠 검사 결과 새로고침 기능 구현
    closes #S08P31A301-440
    
    
    Former-commit-id: 4cbfb22c7fd6f6c5029f929a8b5cd0b6b00c46ba
    Former-commit-id: db15b46041bb75f248056de8d0e807691adb83de

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad986a9f6ca02f230bb7ca819cc0456abe2c8c56daed4950bce93138bc0e6131
-size 1976
+oid sha256:37d1719f0d5c94e22835a37c0301760e99ad1e15b8d6d340ee1caec0925fc6d9
+size 1850
