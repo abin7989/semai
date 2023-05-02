a561df49b63918bf1327c3edc5f4f060a10e99ce
shpark0913 <shpark097754@gmail.com>
2023-05-02 11:05:15 +0900

    :sparkles: [FE]대시보드 SSE 연동 완료
    - SSE가 바뀌면 화면도 변하도록 구현
    
    closes #S08P31A301-435
    
    
    Former-commit-id: 0f18e2b50637051279a6d1513c89cde1191cfee5
    Former-commit-id: ef9ae067359bf04fea1e227d109037adb409fb86

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd7ac390d7daf8f4a137cee7c4cbbf62c12034b6e210f9fd81b28ce1fd9b3c5e
-size 3570
+oid sha256:0751fbd26a536949e0e03710b457d0666d3379fa33823ccb0990a0e81eaa5d46
+size 3575
