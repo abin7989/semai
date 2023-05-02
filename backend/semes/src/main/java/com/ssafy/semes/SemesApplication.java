6702bb321a1ba6cc51070c06e5e3ecd9d9f3097c
안태윤 <cfdw011@gmail.com>
2023-05-02 17:50:17 +0900

    :bug: [BE] OHT 검사 요청시 AI 서버 예외 사항 처리
    500Error, 응답에서 무시할 데이터 처리
    응답 메세지에 따라 예외 메시지 로그 추가
    
    #feature-S08P31A301-407
    
    
    Former-commit-id: 4ba9c0d9a072389c8003a45b69d6f510b2bf3a37
    Former-commit-id: b443d9cf393af2ba44acec133e6c7af2afcd96ca

--- backend/semes/src/main/java/com/ssafy/semes/SemesApplication.java
+++ backend/semes/src/main/java/com/ssafy/semes/SemesApplication.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c54363cd08155f1e70f533feb718d8dc231fa4ecb59d24002829de6297462a7c
-size 685
+oid sha256:1bdab56a75c0ea2dc3ac6708622e553e892e425f865b1e5be8c0eef92ec1bc03
+size 641
