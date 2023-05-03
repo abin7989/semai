dc5676c62021d587d565eae7424e228cfe13ff35
김영식 <kimyoungsik7@naver.com>
2023-05-03 17:11:11 +0900

    :memo: Fast API : 분류 모델 업데이트 및 모델 정보 문서 추가
    closes #S08P31A301-478
    
    
    Former-commit-id: b6f0bc5fd8cc0f8ca1eba34fa4d211d53c67dc07
    Former-commit-id: bd7b67e0582dd344aac54a584da675f6ba8f7a34

--- frontend/src/components/ReportPage/PaginationComponents.tsx
+++ frontend/src/components/ReportPage/PaginationComponents.tsx
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1b6781ec17e00006b43fe6d0fdc56226059aa47e479709a02bc433f8bf618711
-size 133
+import React from 'react';
+
+function PaginationComponents() {
+  return (
+    <div></div>
+  );
+}
+
+export default PaginationComponents;
