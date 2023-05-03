8b030bdffaa127ae3ed12a5b0be15caa22a10b7c
abin7989 <abin7989@gmail.com>
2023-05-03 16:40:04 +0900

    :sparkles: [BE] 소켓 통신 및 알람기능 구현 S08P31A301-456
    Former-commit-id: d2c511d7b62c295f2424124243945386af81535d
    Former-commit-id: a867e5dc1797c6db7f74e8a5c6a5d37e17b37d38

--- backend/semes/src/main/java/com/ssafy/semes/ohtcheck/controller/OHTCheckController.java
+++ backend/semes/src/main/java/com/ssafy/semes/ohtcheck/controller/OHTCheckController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba168c98125d393715e7bc0a974ea6c66313a9a421e080e549e693b236516603
-size 2579
+oid sha256:de0e22c5aac566ee6b8c967cbcade4b7a2cb334220e98be75cff9e9caecaf3fc
+size 2882
