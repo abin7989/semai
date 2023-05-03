12b7ebc2440ae06e6e984ad708a01b5f4027e84d
abin7989 <abin7989@gmail.com>
2023-05-03 16:40:04 +0900

    :sparkles: [BE] 소켓 통신 및 알람기능 구현 S08P31A301-456
    Former-commit-id: d2c511d7b62c295f2424124243945386af81535d
    Former-commit-id: fa267625c8563fd7a55dce0b48a04798c1f0c58c

--- backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/WheelCheckResultDto.java
+++ backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/WheelCheckResultDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44e3fb5a9f8a9daf0af493d3c0a9f1d6e7562582af6e3a984ea4f5cbd84ddeee
-size 2638
+oid sha256:fc99be065e36ccce54acf7d43874b3d3bedb71a0f0a8673b199a85ca016e7fbf
+size 2594
--- backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/service/WheelCheckServiceImpl.java
+++ backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/service/WheelCheckServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e13a89b9950bf1c97cbda366c0aed4de8e79f1be1620e5d692a94303814c58f8
-size 3751
+oid sha256:ed78bff6679a6b65ff639cc1e76b091d7be08e4c0e468816b7b7fe1809bbd10c
+size 3771
