8a72a83406f2a2ec5f458bf1fddc67953da81a7d
안태윤 <cfdw011@gmail.com>
2023-05-03 10:49:59 +0900

    :recycle: [BE] 하드코딩된 볼트 전체 개수 변수로 저장
    NG 결과 계산위한 전체 개수 변수로 설정
    
    #feature-S08P31A301-405
    
    
    Former-commit-id: 04a5945778d109d6ee6e93e98e55ffafc6f76e7a
    Former-commit-id: 492fea0a4e2c36c5df3e398aea07b7d0e1940024

--- backend/semes/src/main/java/com/ssafy/semes/ohtcheck/model/service/OHTCheckServiceImpl.java
+++ backend/semes/src/main/java/com/ssafy/semes/ohtcheck/model/service/OHTCheckServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5a639b5e1058a62fea0bd3500785789adc438d5384206083278a4ccc9e0f406
-size 1986
+oid sha256:45f6fa6dfeb92dec34f85d7411539937fa1afeab2681937023fa0db40a05e481
+size 2070
