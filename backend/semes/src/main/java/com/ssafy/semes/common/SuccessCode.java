10c8a83244eaf8be51d31ccb7db3eacd9f855fa7
안태윤 <cfdw011@gmail.com>
2023-05-04 14:36:02 +0900

    :feature: [BE] 전이학습 시작 API
    전이학습 시작요청
    모델 교체 시 전이학습 결과 DB에 저장
    
    Closes #S08P31A301-468
    
    
    Former-commit-id: c4ef9b2a6c4e193bb1c25c225e139ed4217d6c16
    Former-commit-id: e76022ba591936781f48e476a368c406f9578116

--- backend/semes/src/main/java/com/ssafy/semes/common/SuccessCode.java
+++ backend/semes/src/main/java/com/ssafy/semes/common/SuccessCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:762fe7b7d0b15ee4f6c99ff4f721ef79cd8933ea80b491f67084dd3c58411259
-size 1328
+oid sha256:1bab0226788889961299fa20c86fb50d157a7ddb12a63a69e2a96e528a589c38
+size 1310
