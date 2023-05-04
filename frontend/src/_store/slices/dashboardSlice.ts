c1d6e9ef2c25a2f80077ed937155cb69c6d76a9e
shpark0913 <shpark097754@gmail.com>
2023-05-04 11:03:24 +0900

    :recycle: [FE] Dashboard 코드 리팩토링
    closes #S08P31A301-440
    
    
    Former-commit-id: 77f6627a49cf9019d7c25b4833255317bf854d7d
    Former-commit-id: d95fa64ee8f18cd878103bf04ff7911051a75c4d

--- frontend/src/_store/slices/dashboardSlice.ts
+++ frontend/src/_store/slices/dashboardSlice.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebd02588e31a67f4d447126f868b901f4d19869c46ee68de05e97ea26eb88b16
-size 417
+oid sha256:2be7c832fe39d8f239c3c356089fea6d4ef49b840b1fb14e95b20222143ed929
+size 527
--- frontend/src/components/DashboardPage/OHTResultMenu.jsx
+++ frontend/src/components/DashboardPage/OHTResultMenu.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d143fbc571d40ff2119051ff16988980792525cb3f4ced1e0f4e8033f3a4e86e
-size 4772
+oid sha256:02e26c27db6ce862b483c644fa593bb023b3e67837a18ed1f7104dac7a0f23e1
+size 5044
