f81422c82efb1d4f72f3b2c38f5cff640477d21d
abin7989 <abin7989@gmail.com>
2023-05-04 14:19:02 +0900

    :sparkles: [BE] sse 시간 변경 S08P31A301-456
    Former-commit-id: b7eaac321939c070d49ce7e5e447ea983c123e5a
    Former-commit-id: 6e462cf380e917cb7fb91596b98ba2d4b0fc7ea5

--- backend/semes/src/main/java/com/ssafy/semes/user/controller/UserController.java
+++ backend/semes/src/main/java/com/ssafy/semes/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fb59545f9e6f72336a1e876fa381d4d77a92f46114de09a6a72aa5b2f67fb96
-size 1723
+oid sha256:0a7c06d97dbb5498b0e34151d5cee870dc10f1ff12a6ee7bbe6301b12bf40e2d
+size 1717
