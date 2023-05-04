c41cd7be5636fd02bb9de72e1258af75a9e629e6
gyur1kim <qri1114@gmail.com>
2023-05-04 15:06:59 +0900

    :bug: [FE] 사용하지 않는 페이지 삭제
    - detail 삭제
    
    closes #S08P31A301-
    
    
    Former-commit-id: e19d21f4708f5390ea965a5ceb2b9d1eeb2b929f
    Former-commit-id: 7ad59ef56442b7c2e06ea2cd61ca5b89772020ab

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1beaa76d9209e78c4bbe9ab75023a1b8755be13abb8fd39e5f76c3e470ab615
-size 1936
+oid sha256:c935ebc5fd5d92d0d700381cb8e8a59c6caaaec7e0ab32c94a3ec8b0b783781e
+size 1739
