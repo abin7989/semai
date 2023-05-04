426800c692b49c77470e597bb7d0a85fcd82fed5
gyur1kim <qri1114@gmail.com>
2023-05-04 10:58:00 +0900

    :sparkles: [FE] 레포트 디테일 페이지 라우팅
    - route 생성
    - 클릭 시 이동
    
    closes #S08P31A301-431
    
    
    Former-commit-id: 3eb0fcec7b5e275d24a252c635753a99d053422e
    Former-commit-id: 62bde167df55921a7af5463a551ba44987093e3f

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7917e2edb6a7ed45fc2e2707293719265b9cdc97723fe9def30d34f2ae22e53c
-size 1840
+oid sha256:022afb0d078e3c0457765167d7ab3b335e50261351bf25187b68a4f6eb155107
+size 1985
