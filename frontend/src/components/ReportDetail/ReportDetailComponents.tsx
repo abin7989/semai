5800f53f9a286cb76bb13365eae9ecec6aaa4b01
gyur1kim <qri1114@gmail.com>
2023-05-04 11:30:59 +0900

    :sparkles: [FE] detail route 설정중
    - 설정중
    
    closes #S08P31A301-431
    
    
    Former-commit-id: 9c5166846ccfe715e65833e8789d2c09cabb7ebb
    Former-commit-id: f4c759dad085080773392fc391a217217c31d1cc

---
+++ frontend/src/components/ReportDetail/ReportDetailComponents.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:438572b9862acdabbb050198c97c6c96d42845a2687d1d459d5dfe4a2d98dae7
+size 291
--- frontend/src/pages/ReportDetailPage.tsx
+++ frontend/src/pages/ReportDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfde350deb73d714b4cb36f07843953ba83116f4e8a0b355bf8a1ed936d17fee
-size 1188
+oid sha256:8d37e83e925cbe5744d7ec460e65a2ca74c20f7881992ff6853721b83b40b0a7
+size 1322
