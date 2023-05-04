d9f12b914753dc504a015a5921f501017e29b6cc
gyur1kim <qri1114@gmail.com>
2023-05-04 14:26:00 +0900

    :sparkles: [FE] 자식 라우팅 설정
    - report/:wheelCheckId 를 통해 디테일 페이지 나타냄
    
    closes #S08P31A301-431
    
    
    Former-commit-id: 830270256f30a59bab782fb8ae45d29626ec4cdf
    Former-commit-id: ab158a2bfed5c88d90e2a599343a003e91b78728

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:022afb0d078e3c0457765167d7ab3b335e50261351bf25187b68a4f6eb155107
-size 1985
+oid sha256:733bcb07dd6d17ba7cbbc2bd483f67955f7ab873b9ab4506d8cf90ad7aa685dc
+size 2043
