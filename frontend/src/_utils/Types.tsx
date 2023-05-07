a80703d85e7607b3ad5867611304fdbfdc60da45
gyur1kim <qri1114@gmail.com>
2023-05-08 01:16:43 +0900

    :lipstick: [FE] 레포트 상세보기 표 왼쪽에 띄우기 성공
    - 클릭 시 axios 요청을 이용해 값을 받아옴
    - 값을 detail info에 담아 props로 전달
    
    closes #S08P31A301-482
    
    
    Former-commit-id: 03634c74936f79e070ca69eebf7d404b29e1fbd2
    Former-commit-id: 9e6e7ba8aeb97ce14200bfe02e24eb4bba841b80

--- frontend/src/_utils/Types.tsx
+++ frontend/src/_utils/Types.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98a36c04ea057888bb2d5baae725e1601953c7eaea7dd595cc417ce2205d4367
-size 1486
+oid sha256:db0ce15f3fb4263a92154da403f2155e53d9242733e3344348d16c2bccd2dba1
+size 1508
