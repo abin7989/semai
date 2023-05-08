d66f8ae0bafcb4fdcc935429cfb518d7020b9c0b
gyur1kim <qri1114@gmail.com>
2023-05-08 16:58:18 +0900

    :sparkles: [FE] report table 스크롤 기능
    - table을 감싼 div태그에 높이를 주고, 높이보다 커지면 스크롤되게 설정
    - thead와 tfoot에 sticky 속성 지정, 스크롤시에도 움직이지 않음
    - border-collapse: separate와 border-spacing을 이용해 border가 움직이지 않게 고정
    
    closes #S08P31A301-495
    
    
    Former-commit-id: ca83c4fb692ed3ceaa68e32966b0e5672f9d0d6c
    Former-commit-id: 01194e068dca17fb97b20a69a26508ed33edf232

--- frontend/src/components/TableComponents.tsx
+++ frontend/src/components/TableComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5f521f996e02c82c7b860873273c9e208cf966ea080b3f532cd265042c05a0e
-size 2349
+oid sha256:16e05ac93b6e32c0f5d44d653dff8b06c53ddce103af31870ed2ec847de085ab
+size 2840
