a6ad960785f07f5d2a9cd39f51261f157b328540
gyur1kim <qri1114@gmail.com>
2023-05-08 17:00:27 +0900

    :lipstick: [FE] pagination padding 삭제
    - padding: 0 지정
    
    closes #S08P31A301-
    
    
    Former-commit-id: 65993c84886dd73b7c35c0695728e047bd904360
    Former-commit-id: 82869c57c4afae2c79d963e7883ec0d5a00ce7fb

--- frontend/src/components/ReportPage/PaginationComponents.tsx
+++ frontend/src/components/ReportPage/PaginationComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0be10d204432f2c7bb2849ce9ba8cdf693b272e143a146013bbcb3b642925c12
-size 1727
+oid sha256:d7e4f99cb47326a8ac402216f284b7181a3e5a90a26883b3c01f1dcf3d4ce87e
+size 1741
