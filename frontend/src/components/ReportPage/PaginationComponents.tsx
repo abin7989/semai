9806cb9fd57b15c34bd1631b38eac024273393b7
gyur1kim <qri1114@gmail.com>
2023-05-08 16:07:20 +0900

    :lipstick: [FE] --shadow-color 변수 지정
    - pagination과 closeButton에 해당 색상 지정
    
    closes #S08P31A301-495
    
    
    Former-commit-id: c41cdd154aba5f32de902aeb6c161f0761235d41
    Former-commit-id: bcb0856ada66206c5aae2ea5b54731dfbe8de87f

--- frontend/src/components/ReportPage/PaginationComponents.tsx
+++ frontend/src/components/ReportPage/PaginationComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e2a7865e69d15ada7ab3584cf04fddc576d676c81c4a38d4ef6c4814e53cb69
-size 1732
+oid sha256:0be10d204432f2c7bb2849ce9ba8cdf693b272e143a146013bbcb3b642925c12
+size 1727
