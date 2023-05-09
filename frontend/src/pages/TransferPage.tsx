bf2c5cab785d3532fcaa5f9a7dd9070817cf7dc5
gyur1kim <qri1114@gmail.com>
2023-05-09 14:12:38 +0900

    :fire: [FE] 사용하지 않는 파일, 코드 삭제
    - 이미지 디테일을 모달로 띄우지 않음
    - 모달과 관련된 코드, 파일 삭제
    
    closes #S08P31A301-517
    
    
    Former-commit-id: ea054b68cdcda308ceb015bbb5af043e5c6f91e9
    Former-commit-id: bfa819b50ad6a2a92e402bcde1a96903b25cfec3

--- frontend/src/pages/TransferPage.tsx
+++ frontend/src/pages/TransferPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd886def645a8b3b7be306496a725ce5b9763eda67cf9d538c406557800558c8
-size 1470
+oid sha256:761de21664612191890cdc72aed873ad91d4886493e8a76ca0a9ee627eecb68c
+size 501
