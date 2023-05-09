3d60e06f7d98c83aa2f9dfaba43a9b4d09d1b82b
김영식 <kimyoungsik7@naver.com>
2023-05-09 17:54:53 +0900

    :sparkles: 풀림탐지 on off 기능 추가
    closes #S08P31A301-563
    
    
    Former-commit-id: fa9fc49797352f2e60d49db7aed0d427d31c81ca
    Former-commit-id: 4529e4d1f21aeed1e3342c3d305022660f1750ee

--- ai/api/classification/RegNet.py
+++ ai/api/classification/RegNet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a28ce2e1eb6912c7f5604f062091a2218e7ad87987b97ded2d5d707a1eca029
-size 5158
+oid sha256:1bb6b6fccee241dc6d6563e8332a0da228cf2d5eacb4b56d1ad956ed4234cf0d
+size 6967
