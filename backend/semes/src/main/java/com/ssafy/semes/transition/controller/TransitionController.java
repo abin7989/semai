90140e5b08a47458ff262fb274463daff813a5eb
안태윤 <cfdw011@gmail.com>
2023-05-09 17:35:15 +0900

    :feature: [BE] 이미지 조회 API 수정
    이미지 최대 개수 100개로 제한
    전체 목록 정상, 유실, 파단 세개로 제한
    
    Closes #S08P31A301-539
    
    
    Former-commit-id: 93705980703b02ed76dca99a9d688f5ae17e07a4
    Former-commit-id: f677732c136d680601e3ea326831da196172a512

--- backend/semes/src/main/java/com/ssafy/semes/transition/controller/TransitionController.java
+++ backend/semes/src/main/java/com/ssafy/semes/transition/controller/TransitionController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aaec11891a49b9eede81795e99454e3df7612743746560b3379c19e6c60932b0
-size 3270
+oid sha256:628a8f4997558bc0d338a9501feb647c3eaad25192c97f13d3c541f4ce12b86d
+size 3272
