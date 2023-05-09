09c740f5984f63f88a64389f65af28b5ea569d49
shpark0913 <shpark097754@gmail.com>
2023-05-09 14:00:33 +0900

    :recycle: [FE] Dashboard 코드 리팩토링
    closes #S08P31A301-502
    
    
    Former-commit-id: f811df24cf842fe89fc6b66d6ab849dfa51f7f50
    Former-commit-id: 02358d6b5a8e04351c66fa823b369a535031d1d1

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a13d4f0a06c75ba215de9149898241624163da795f12b1850daddc8cc0fc7097
-size 2968
+oid sha256:40dc055b0011482e0d321ce32059f9aff68964d2a529c8b5d1b14623ed664d3a
+size 2987
