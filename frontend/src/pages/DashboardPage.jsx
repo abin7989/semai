688281694586e14522dcf80fca5a3dec3d01f14e
shpark0913 <shpark097754@gmail.com>
2023-05-09 13:36:41 +0900

    :recycle: [FE] Dashboard 코드 리팩토링
    - 주석 삭제
    
    closes #S08P31A301-500
    
    
    Former-commit-id: c21b7db3fcc945d83d8286aa374ce3f6a5f9b87c
    Former-commit-id: 7034142abeb095167a1e4a976707ea70f86e0baf

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5e7eb33d8881b86b07a44ea6d3ea05717a3e1cd1b8488bd7c22cfdacd83400d
-size 3518
+oid sha256:a13d4f0a06c75ba215de9149898241624163da795f12b1850daddc8cc0fc7097
+size 2968
