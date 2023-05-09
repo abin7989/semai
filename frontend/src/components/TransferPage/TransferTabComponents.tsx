5060beb354e49f7f2303e04faa9717a55a838788
gyur1kim <qri1114@gmail.com>
2023-05-09 16:50:48 +0900

    :sparkles: [FE] 이미지 디테일 닫기
    - 이미지 디테일 닫기
    
    closes #S08P31A301-517
    
    
    Former-commit-id: b7bb6e824951c2a775522dbd0a9a4f8c49068d66
    Former-commit-id: 6e8a42eae959d2377aece8eef0438e05a2d9de16

--- frontend/src/components/TransferPage/TransferTabComponents.tsx
+++ frontend/src/components/TransferPage/TransferTabComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2258fd815b738607353c0c7dc00b1cd9d59eb3787f881ca19ced24a63c4d496e
-size 1747
+oid sha256:96941e3685ac6d61114dcc023b8f7282ac89b4d59f99ce97890b245842f67a62
+size 1777
