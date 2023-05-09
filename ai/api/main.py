92a5fedd78fe13dbabca5a0a53950bb67f49631b
김영식 <kimyoungsik7@naver.com>
2023-05-09 17:54:53 +0900

    :sparkles: 풀림탐지 on off 기능 추가
    closes #S08P31A301-563
    
    
    Former-commit-id: fa9fc49797352f2e60d49db7aed0d427d31c81ca
    Former-commit-id: 3dc1cc7ef716111cb100c3f7b1fcb116969ac4c2

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5caa1c26ca2a03ccd28df4d5ec25b9f03688c319c788f4c6fb771032b126fdcb
-size 3326
+oid sha256:476e7c0a3aaebaa4d7ca26fdb69657f8e9c6760e35363acf719fe2dce73cc632
+size 3347
