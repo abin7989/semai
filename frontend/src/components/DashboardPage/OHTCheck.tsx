81dd120c1e2658f15d6f00f578f065ba5ecab190
shpark0913 <shpark097754@gmail.com>
2023-05-09 16:38:24 +0900

    :sparkles: [FE] 현재 검사 OHT api 연동 완료
    closes #S08P31A301-502
    
    
    Former-commit-id: 9e345555801cebff804f38ea29b4bb9c50103831
    Former-commit-id: 758f6df4621bb88246849e76e38944c207816edf

--- frontend/src/components/DashboardPage/OHTCheck.tsx
+++ frontend/src/components/DashboardPage/OHTCheck.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2b9f87120e4c2af124d5e86310a09bc74d382a68c817049c2f06533cb0e53a5
-size 2933
+oid sha256:7caa2d59b8d84713bb31eea34eb301667a6f893b20c9eb0850f8772b4365aa6e
+size 3063
