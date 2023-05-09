22d94ccd399e6d3176b82c37bc7ed13fc6b1fa84
shpark0913 <shpark097754@gmail.com>
2023-05-09 13:36:41 +0900

    :recycle: [FE] Dashboard 코드 리팩토링
    - 주석 삭제
    
    closes #S08P31A301-500
    
    
    Former-commit-id: c21b7db3fcc945d83d8286aa374ce3f6a5f9b87c
    Former-commit-id: 5660875db08ba150530f3c8d872c736d99b50739

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63efaf85c3753589ffad941214f77974e2682a082a8dc4e9e0bdacfa67e143d2
-size 5757
+oid sha256:ce45721945e90c1cd34e5e0190f87405d2ba016cf9772c78f3b7cf1079962f4b
+size 5898
