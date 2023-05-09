64f6f61aa32f52add5409e697caa1530b5eb5d92
gyur1kim <qri1114@gmail.com>
2023-05-09 14:07:38 +0900

    :sparkles: [FE] 제목 클릭 시 오른쪽에 이미지 띄우는 영역
    - isDetailOpen이라는 state를 이용해 관리함
    - open됐을 때 classname을 바꿔 렌더링을 다르게 한다
      - 이미지리스트와 디테일 이미지를 grid로 관리
    
    closes #S08P31A301-517
    
    
    Former-commit-id: bba575f04f23f52766f90f7db29fccf676667e1e
    Former-commit-id: 42da7d5c6bc64a9e118efa624e9205dea4423a17

--- frontend/src/components/TransferPage/TransferImageComponents.tsx
+++ frontend/src/components/TransferPage/TransferImageComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aecd70517d38a25db5ce284368c855914a254808f7a143b274e40ad3b7ca0a56
-size 751
+oid sha256:6fe22df55a429c49607ec14a6e33e17b88508ae966676a9cfa1f1059a7078888
+size 862
---
+++ frontend/src/components/TransferPage/TransferTabComponents.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2258fd815b738607353c0c7dc00b1cd9d59eb3787f881ca19ced24a63c4d496e
+size 1747
