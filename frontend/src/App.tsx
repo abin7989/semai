601209f0e5b443f47bc0d41eebc248de90eb908b
gyur1kim <qri1114@gmail.com>
2023-05-09 13:10:18 +0900

    :sparkles: [FE] 전이학습 볼트 이미지 띄우기
    - tab menu의 content로 이미지 띄우기
    
    closes #S08P31A301-514
    
    
    Former-commit-id: 0a1899adb90af9d20804b6310b404f26c213f8b0
    Former-commit-id: d5f33e69c0ab78063f413e6263832d0a57ea33fb

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60f4a058a3b58da74495d73154a6d0db585d25c3c6f08b4f2c86093edfd0e206
-size 1443
+oid sha256:0f84a75a12c7b9ed624f6fa8ebdc709a7a6d49a130dfb2a6367417fa158886e5
+size 1467
