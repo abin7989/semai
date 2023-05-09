d1bc097a55e130399fd3172e1c757e11d2fedc10
gyur1kim <qri1114@gmail.com>
2023-05-09 17:11:51 +0900

    :sparkles: [FE] 이미지 디테일 우측에 띄우기
    - isDetailOpen state를 활용해 띄우기
    - open일 때 클래스이름을 변경하여 grid에 보이는 너비 조절
    - 닫기 버튼을 통해 디테일 닫기
    
    closes #S08P31A301-517
    
    
    Former-commit-id: baed0f858f51f4e1f7d3c1915c2316664fb054f7
    Former-commit-id: 9635f0121a2091e6259f817d950f23c86ace7645

---
+++ frontend/src/components/TransferPage/TransferBoltImages.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bb133161ab3e03553d1ccb3d244adfb185853271f310fb0d6c82de6c45b3fe28
+size 3198
--- frontend/src/pages/TransferPage.tsx
+++ frontend/src/pages/TransferPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:761de21664612191890cdc72aed873ad91d4886493e8a76ca0a9ee627eecb68c
-size 501
+oid sha256:31d02d370e707b8b4332771d71cf4a30df395adf220d5e95be8f9c56dd3979f8
+size 1656
