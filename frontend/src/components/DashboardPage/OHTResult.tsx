b8daf89088e9fc96a4826b5619d152e4090cd97b
shpark0913 <shpark097754@gmail.com>
2023-05-10 13:13:43 +0900

    :sparkles: [FE] OHT 휠 검사 결과 api 연동
    closes #S08P31A301-501
    
    
    Former-commit-id: 4e7d2343e1c5239699e9e37895fc53d2125f9d4c
    Former-commit-id: b3dfbaa9becc97a53e37fe461511eefa52caead8

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b87e985127e62c8d626b53808076c5b85f3efaa2a3cee049071ca237ee186f26
-size 5971
+oid sha256:b5012c13dacca0b25f040dae906f1a7f3b4a8fd8c7ddaf1caf573a253761af04
+size 6024
