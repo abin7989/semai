90a973a07d97fd4b08c2c4ed16b90c7309536515
gyur1kim <qri1114@gmail.com>
2023-05-10 16:48:04 +0900

    :sparkles: [FE] 이미지 클래스 이동
    - 현재 양호에서 유실로 이동
    
    closes #S08P31A301-520
    
    
    Former-commit-id: 47f900dc4f780df8ea6ae5e2921a679b0860a268
    Former-commit-id: f20d315bf5d9619c2da57723767fdc7209e83102

--- frontend/src/_utils/Action.ts
+++ frontend/src/_utils/Action.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5faae26c14dbdfc955d1953abbee090b30556fcbb92f26c73b238f147ee6e57b
-size 848
+oid sha256:0bde0e8d81e56055e4e06b1c74263f3d3be2d65f28a2c2b33e0a7c385b8da591
+size 1022
