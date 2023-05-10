58231d29db1c061c0fb7bd984886832c9d06a513
안태윤 <cfdw011@gmail.com>
2023-05-10 11:00:11 +0900

    :recycle: [BE] 대시보드 메인 이미지 url 누락 데이터 수정
    ResponseDto에 image url 추가
    
    Closes #S08P31A301-557
    
    
    Former-commit-id: a5b0f93ce34f7ad5ee406ab2bea0f9e970d132c5
    Former-commit-id: 83ec4d62e0068ad8d6c34743f48e112f4decc624

--- backend/semes/src/main/java/com/ssafy/semes/dashboard/model/service/DashboardServiceImpl.java
+++ backend/semes/src/main/java/com/ssafy/semes/dashboard/model/service/DashboardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:994495627382fd6ff6daf97f15bddc41b0be4cd30c4d6452585eef6666e75870
-size 3587
+oid sha256:fb6d24e5b1da15dab87df754ad8818768b4995d57be14605e15ccb77d1ccb285
+size 3635
