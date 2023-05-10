d298098640489afd395d2f9a26d8c9c95d683070
gyur1kim <qri1114@gmail.com>
2023-05-10 16:09:53 +0900

    :sparkles: [FE] 이미지 선택 기능
    - input checkbox를 이용
    - e.target.checked 값을 이용해 선택됐을 때, 선택되지 않았을 때로 나눔
    
    closes #S08P31A301-520
    
    
    Former-commit-id: 23c2026531f78eb1b88c80c04ecf2e61e652175d
    Former-commit-id: 5bf5770767f70acab7141e727144ae016799f991

--- frontend/src/components/TransferPage/TransferBoltImages.tsx
+++ frontend/src/components/TransferPage/TransferBoltImages.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48a5745572bb664af2854e835d565af4e4bc53985269cd2a04bde0fa0b71cf5e
-size 3605
+oid sha256:af749f70ee7825449ada3394bfa251ae076a59daab84cbd22ff5f5a4d229a971
+size 4646
--- frontend/src/components/TransferPage/TransferImageComponents.tsx
+++ frontend/src/components/TransferPage/TransferImageComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:635fae1cb6a6e4b477923627a713deefadaf2b87f9f13c8fc06c395b9e1252c4
-size 904
+oid sha256:a228b5a2de8683e760401b409569849bff8642bae452b703a839e4950a308e3b
+size 1066
