8c03a3cdb4309529b02bd1a43f461380c95ed9f2
shpark0913 <shpark097754@gmail.com>
2023-05-10 10:33:42 +0900

    :recycle: [FE] 사용하지 않는 로그 출력 삭제
    closes #S08P31A301-501
    
    
    Former-commit-id: 955da4627564a829ec0f145104bfed7c5f3f4aca
    Former-commit-id: be3a1764e0eac82ee3c487157f38b3a0bc0af184

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90d3e83714b8a150a2f9cefe6f45a87eab5c8b5ccfb589e92207e3902906d7b7
-size 6022
+oid sha256:b87e985127e62c8d626b53808076c5b85f3efaa2a3cee049071ca237ee186f26
+size 5971
