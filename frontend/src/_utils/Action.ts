12667091aefcb64c8c3a4662be511a0a9b351849
gyur1kim <qri1114@gmail.com>
2023-05-10 13:30:28 +0900

    :sparkles: [FE] 볼트 이미지 form 생성
    - set이 아니라 append를 사용해야 form에 새로운 key와 value가 추가된다
    
    closes #S08P31A301-519
    
    
    Former-commit-id: 50eec1598a33a264de755fc9fa2b63cc65319e7e
    Former-commit-id: e2ad0780895323e5b9280dc8d8efd34e14b7003c

--- frontend/src/_utils/Action.ts
+++ frontend/src/_utils/Action.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93c119f650415c350b755ce1850711bbda34291c3de59e5502986ade77a05564
-size 821
+oid sha256:5faae26c14dbdfc955d1953abbee090b30556fcbb92f26c73b238f147ee6e57b
+size 848
--- frontend/src/components/TransferPage/TransferBoltImages.tsx
+++ frontend/src/components/TransferPage/TransferBoltImages.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9f9038721e1a95c9ef70c7b5fd6be35bb203c85b31fb7338698be619a97234d
-size 3304
+oid sha256:c169abc265b62a13ba350fe15dd467d061e2f8f6ae3f2fa1830b4c3b7ecbdb6e
+size 3478
