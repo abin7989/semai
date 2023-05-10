059fb9f08bfc0646fdc84fc1d578f5d650e5d3f6
shpark0913 <shpark097754@gmail.com>
2023-05-10 16:49:14 +0900

    :recycle: [FE] Dashboard 로그 삭제
    - 사용하지 않는 로그 조회 삭제
    
    closes #S08P31A301-505
    
    
    Former-commit-id: 115128736d403800369970a6ab126433da79ef57
    Former-commit-id: 58d7abc0f3ccf00cbc1a374c6acd04fa68682c88

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25545a3d42dbd40164ebcb1afdc0ac8f7279323be566d73c57385d72a1c4b20b
-size 6425
+oid sha256:d2db4c5a94f4654bd29c67d30a1b9e4aed0fe57abafda2efba24956f9bbdd5fd
+size 6351
