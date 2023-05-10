75a6ba2a8ec8e41ea37c7a97935f45200bfbfd12
shpark0913 <shpark097754@gmail.com>
2023-05-10 10:33:42 +0900

    :recycle: [FE] 사용하지 않는 로그 출력 삭제
    closes #S08P31A301-501
    
    
    Former-commit-id: 955da4627564a829ec0f145104bfed7c5f3f4aca
    Former-commit-id: d552b5b4a907cd99fceadea9e2755e7fa808f80b

--- backend/semes/src/main/java/com/ssafy/semes/dashboard/model/DashboardMainResponseDto.java
+++ backend/semes/src/main/java/com/ssafy/semes/dashboard/model/DashboardMainResponseDto.java
@@ -1,22 +1,3 @@
-package com.ssafy.semes.dashboard.model;
-
-import com.ssafy.semes.image.model.ImageEntity;
-import lombok.*;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
-@Builder
-@ToString
-@AllArgsConstructor
-public class DashboardMainResponseDto {
-    private LocalDateTime ohtCheckDatetime;
-    private LocalDateTime ohtChangeDate;
-    private String oht_sn;
-    private int boltGoodCount;
-    private int boltOutCount;
-    private int boltLoseCount;
-    private int unclassifiedCount;
-    private String wheelPosition;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e53581dbac406889825c863bde9c46c60b8bd57946a8e3c3ff15a3e801ba5514
+size 511
