13de90e34fe1421c49ca2cc79492e4b9bbd48e3f
shpark0913 <shpark097754@gmail.com>
2023-05-10 15:01:01 +0900

    :sparkles: [FE] OHT 휠 검사 결과 이미지 비정상일 시 빨간 테두리 부여
    closes #S08P31A301-505
    
    
    Former-commit-id: f5d3eb374edeedd831af371c702b5f068af80bc3
    Former-commit-id: 640687e7ce439c651df27b272507faa31a5553d3

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21760384a62bd9ce75169a92cdf6d285986e387d4264190ed11da0036edd4cf1
-size 6069
+oid sha256:8d1515d50afff082f99a1832a1e67bec52002641785faccb7a18123525b1839d
+size 6256
