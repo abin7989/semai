d612470f6ba9cd4bd34ba8c640922dded9aceda3
abin7989 <abin7989@gmail.com>
2023-05-10 10:05:07 +0900

    :sparkles: [BE] SSE 휠 점검 오류 수장 S08P31A301-558
    Former-commit-id: 46a1944d46f501a9fba2df4a587357893b613c0e
    Former-commit-id: 3d5b01cf4e46dc323d4dd91f4ed812a358ec5665

--- frontend/src/_hooks/useBodyScrollLock.tsx
+++ frontend/src/_hooks/useBodyScrollLock.tsx
@@ -1,13 +1,3 @@
-import { useCallback } from 'react';
-
-export function useBodyScrollLock() {
-  const lockScroll = useCallback(() => {
-    document.body.style.overflow = 'hidden';
-  }, []);
-
-  const openScroll = useCallback(() => {
-    document.body.style.removeProperty('overflow');
-  }, []);
-
-  return { lockScroll, openScroll };
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b81e303c1f85be62219f1d2507df2ce4bce6350414e578c45792eb2d119f6e99
+size 315
