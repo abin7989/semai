cb97c52e69d2a55407ef863cef285f69f23a4a3a
gyur1kim <qri1114@gmail.com>
2023-05-10 09:52:47 +0900

    :sparkles: [FE] 학습과 클래스별 이미지 분류
    - 서버에서 주는 값의 0번째 idx는 클래스별 분류, 1번째 idx는 학습용 데이터가 넘어온다
    
    closes #S08P31A301-519
    
    
    Former-commit-id: 43c4cef4bf057d50f27941eeb2f0bc4c3fb20d70
    Former-commit-id: 6f087fa5b982e7b7a0686135883d667b41e46ade

--- frontend/src/pages/TransferPage.tsx
+++ frontend/src/pages/TransferPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31d02d370e707b8b4332771d71cf4a30df395adf220d5e95be8f9c56dd3979f8
-size 1656
+oid sha256:052b8685ff03d5d54472564b702f8e8f6751e0ec6c02df9f477730a5b75db6cb
+size 1956
