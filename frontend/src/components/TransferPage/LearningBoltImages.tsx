cb83be6b5c10e4e68eb6770750107ca8c92accc8
gyur1kim <qri1114@gmail.com>
2023-05-10 14:27:48 +0900

    :sparkles: [FE] 볼트 이미지
    - BASE_URL로 이미지 요청 보냄
    
    closes #S08P31A301-520
    
    
    Former-commit-id: 274ba9dd803b3c9a1266f72e5dcc259e64892d4e
    Former-commit-id: 59fb36cfb97f7ff0e8959f5697af955fcfd1835d

--- frontend/src/components/TransferPage/LearningBoltImages.tsx
+++ frontend/src/components/TransferPage/LearningBoltImages.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0565bfc964e3d4e4c28425d1bcedbc3a3225308d8160f0594ea653127e6bea34
-size 3142
+oid sha256:e05fc8a366f93e24bb996d39520e3eeb6320fd907ecaa4cfc05badf3b5072288
+size 3226
--- frontend/src/pages/TransferPage.tsx
+++ frontend/src/pages/TransferPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aae6b619820d54de78fd3be7e61046f8c9f359eea801119c60b8701a4414782c
+oid sha256:19eccca07b63576b3a36eb81ca45a422947b6dc092dd447d53c30a68db312c4d
 size 2197
