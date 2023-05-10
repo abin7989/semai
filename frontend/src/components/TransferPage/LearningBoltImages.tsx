ad57ad5166d566e9e638d0ce79a200113e8e197e
gyur1kim <qri1114@gmail.com>
2023-05-10 10:49:52 +0900

    :sparkles: [FE] 학습 탭 클래스 이름 클릭 시 열고 닫는 기능
    - 이름을 클릭하면 display를 none과 grid로 토글 가능
    
    closes #S08P31A301-519
    
    
    Former-commit-id: 8e4b40de30a872fcc048b269f8a745dcc0e9fa39
    Former-commit-id: b4c12fb1d7a4ad29271bf76a53ebfecfff4c464c

--- frontend/src/components/TransferPage/LearningBoltImages.tsx
+++ frontend/src/components/TransferPage/LearningBoltImages.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba454bc3c571cc1cd9a5fa5dc5a9cc3ec2adb7b8230e1e37453a2a8a528ed606
-size 2793
+oid sha256:0565bfc964e3d4e4c28425d1bcedbc3a3225308d8160f0594ea653127e6bea34
+size 3142
