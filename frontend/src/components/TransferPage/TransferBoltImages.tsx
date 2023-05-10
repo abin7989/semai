14230dace8d7eb6163e50dcbb8c9b45a9bb85a79
gyur1kim <qri1114@gmail.com>
2023-05-10 10:23:56 +0900

    :recycle: [FE] 볼트 이미지 띄우기
    - 클래스별, 학습별 이미지띄우기
    - 학습 탭에서 클래스별로 이미지 나누기
    
    closes #S08P31A301-519
    
    
    Former-commit-id: 6fade152a39b513d508a483baec62cb12d6110b9
    Former-commit-id: 4f27279f8ba3fa9f2f04ce44cd254921c75f8e77

--- frontend/src/components/TransferPage/TransferBoltImages.tsx
+++ frontend/src/components/TransferPage/TransferBoltImages.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb133161ab3e03553d1ccb3d244adfb185853271f310fb0d6c82de6c45b3fe28
-size 3198
+oid sha256:b9f9038721e1a95c9ef70c7b5fd6be35bb203c85b31fb7338698be619a97234d
+size 3304
