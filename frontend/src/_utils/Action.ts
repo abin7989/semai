c41c7a91d37c068b9491038b56b608035f9287ff
abin7989 <abin7989@gmail.com>
2023-05-10 10:45:28 +0900

    :sparkles: [BE] oht 수정날짜 수정 S08P31A301-558
    Former-commit-id: d2a278fb12648971ed2cf0c64f7ad6bcd8748062
    Former-commit-id: ec1abffd7bfd789bda38e91158757d3368c51cd9

--- frontend/src/_utils/Action.ts
+++ frontend/src/_utils/Action.ts
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:71c18a9befcb648eed4fb0a63b70b8ca6cd3615734117c21507ea62dfa1f6fa1
-size 104
+export async function TransferTestAction () {
+  console.log("action 실행됩니다.");
+  return null;
+}
