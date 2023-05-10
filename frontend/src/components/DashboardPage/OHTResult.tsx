fbb07fed0e6dada2128eec98132872c7774aad33
shpark0913 <shpark097754@gmail.com>
2023-05-10 15:32:45 +0900

    :bug: [FE] 점검 시간의 초가 00초일 때 undefined로 출력되는 버그 해결
    - 삼항연산자를 사용해서 해결
    
    closes #S08P31A301-505
    
    
    Former-commit-id: e2f93571aa9fa90ec560c52ac25de952fe7316a9
    Former-commit-id: cbcd2dd34a0f7386ac9c265895b22d985517313a

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d1515d50afff082f99a1832a1e67bec52002641785faccb7a18123525b1839d
-size 6256
+oid sha256:25545a3d42dbd40164ebcb1afdc0ac8f7279323be566d73c57385d72a1c4b20b
+size 6425
--- frontend/src/components/DashboardPage/OHTResultMenu.jsx
+++ frontend/src/components/DashboardPage/OHTResultMenu.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00b976d0a72b22293aef7212a4cfc8bee943d7164ca93f094bf99d3b8312fd9d
-size 5161
+oid sha256:64c9958e875803c83e9bba014f42891f4b25a32e4c23a4aa4cf5d5772e82a208
+size 5315
