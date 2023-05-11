0deac13931e3df8f05cecf906ffe3e7a0c2608d2
abin7989 <abin7989@gmail.com>
2023-05-11 16:49:06 +0900

    :sparkles: [BE] 레포트 테스트 코드 작성 및 N+1 문제 해결 S08P31A301-562
    Former-commit-id: 6c471a61d52c2a6953c4a44509fd18569872ea1c
    Former-commit-id: c6f09725d33b9fdc3cb37a81a03163fc44f0bf5e

--- backend/semes/src/main/java/com/ssafy/semes/report/model/service/ReportServiceImpl.java
+++ backend/semes/src/main/java/com/ssafy/semes/report/model/service/ReportServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7e11b39256adf1b43b6526c34be09aa90c8be7aded89b9e06bdc3a1d6e38ae7
-size 5348
+oid sha256:49ffe70d86a8789003ee0c946419737125a6d064618a7a9b3c4a597c31fa17b9
+size 5371
