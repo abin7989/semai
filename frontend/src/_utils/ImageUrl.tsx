b5eb9439fabf14eee8e2e3b62b5c3262563c2c0c
gyur1kim <qri1114@gmail.com>
2023-05-11 13:54:03 +0900

    :sparkles: [FE] image full url을 리턴하는 함수
    -
    
    closes #S08P31A301-521
    
    
    Former-commit-id: 11d5b0211dcf5d915d506cf7eb3ea67319b90f3e
    Former-commit-id: ab5c273c1f5f9f01a5963deb051314cfa79b495b

---
+++ frontend/src/_utils/ImageUrl.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a1740f9dd96806e3ccd31721d288dbaf7ea9ec752f6ae979af69cb38f267053
+size 152
--- frontend/src/components/TransferPage/LearningBoltImages.tsx
+++ frontend/src/components/TransferPage/LearningBoltImages.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec5a273ede73ac299b9393337763cdb8a15a0b37fb660d438b58c24f5085b971
-size 4068
+oid sha256:3e29cb0fce3bc4606e20af2674bfd63d8cf9773b97c2996af017ba155940eceb
+size 4053
