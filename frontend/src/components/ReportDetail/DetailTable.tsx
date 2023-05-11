bf073d5860189c159d28d169d6f1812fdb1784a8
gyur1kim <qri1114@gmail.com>
2023-05-11 13:54:03 +0900

    :sparkles: [FE] image full url을 리턴하는 함수
    -
    
    closes #S08P31A301-521
    
    
    Former-commit-id: 11d5b0211dcf5d915d506cf7eb3ea67319b90f3e
    Former-commit-id: fb17308a0b84e325282453fb35008a57b2a51d6b

--- frontend/src/components/ReportDetail/DetailTable.tsx
+++ frontend/src/components/ReportDetail/DetailTable.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:feb34ecb41edcae26edd102b0dc8c292b311890150a3aff5cde4b0bbdda710f4
-size 1717
+oid sha256:78da6e05f75e09c0857cade54ae08f80b0e39477a15eedfc143f6e393b07a443
+size 1706
