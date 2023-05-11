4f7f0707ceb7d71701fc0c3a4540e4841a9ae753
shpark0913 <shpark097754@gmail.com>
2023-05-11 13:52:47 +0900

    :lipstick: [FE] NavBar에 설정 아이콘 삽입
    closes #S08P31A301-507
    
    
    Former-commit-id: 859cde904133d8ca5b0d1a4f213b2d766a40d42b
    Former-commit-id: 3f17cd18d966239690afe6d6a6148be4c2ac698a

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1746c94b984603d58002bb57770741092529f0dbbfa61f8ed9d531d34ffa3c44
-size 3679
+oid sha256:1683f47350a8941addccc70129b4afd1293f2e217ce0aa18293623fcbc93802d
+size 5286
