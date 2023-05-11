3f1c4ea949001b52e6092f968f9669789e6486cf
gyur1kim <qri1114@gmail.com>
2023-05-11 16:20:12 +0900

    :sparkles: [FE] tabIndex는 persist에서 제외
    -
    
    closes #S08P31A301-513
    
    
    Former-commit-id: 452cb42915bf500d9b844b6f45f1e0642b9c07b9
    Former-commit-id: 49a554a6f5ca0cfff674fbd359cba0f28f484c6a

--- backend/semes/src/main/java/com/ssafy/semes/user/model/UserRequestDto.java
+++ backend/semes/src/main/java/com/ssafy/semes/user/model/UserRequestDto.java
@@ -1,13 +1,3 @@
-package com.ssafy.semes.user.model;
-
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-public class UserRequestDto {
-    private String userId;
-    private String userPwd;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:23060874168cbd7efe99d9fea8d91c5668eb32ce547ddc03e785c5cd930d7b69
+size 207
