23d6d31cdd7ffe9541d53614b68517ec08d35f3c
shpark0913 <shpark097754@gmail.com>
2023-05-11 16:45:25 +0900

    :lipstick: [FE] 사진 색상에 따라 휠 위치와 X 보이지 않아서 색상 변경
    closes #S08P31A301-508
    
    
    Former-commit-id: 09d9c44b44d97e0b3801ca95c66281595e4fee83
    Former-commit-id: 3cf4214e7da733a854b3e027247df8b24240bc7c

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7674c5adfdf720981e487dbf44d6541ff99ad96a090afb18290fc4953a7adb74
-size 8456
+oid sha256:f249fb02b9b4762d585bb8f7d621f974f0f980463812a6c75fda0cf10699c6cc
+size 8503
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1683f47350a8941addccc70129b4afd1293f2e217ce0aa18293623fcbc93802d
-size 5286
+oid sha256:b6728b16b97cf92f2bb8445d2f9e16ebb20cc4d1ddce361eb000b70e229d4740
+size 5513
