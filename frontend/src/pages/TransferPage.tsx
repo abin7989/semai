fa57b2095b3945e34def0670dea784c38e2547c7
gyur1kim <qri1114@gmail.com>
2023-05-11 17:32:45 +0900

    :recycle: [FE] TabMenu 리사이클
    - state로 관리되던 값을 redux에 저장
    
    closes #S08P31A301-513
    
    
    Former-commit-id: e69e59de8a7aa78719e6c88e6e2ed10a389c2071
    Former-commit-id: 49d50b92b0ff42f068f7f741168d720a93c0c6b8

--- frontend/src/pages/TransferPage.tsx
+++ frontend/src/pages/TransferPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:109f09c94d08d4c0c8c9114e1b46fdc395daeca7c6da8147708c0da5bc8892be
-size 1720
+oid sha256:2946a8edd01201042476b28edb0d350e675141a6a45cb5f5006aad7afe0797c1
+size 1582
