48dfd789f88a96aee2285b3505c4f85086961804
gyur1kim <qri1114@gmail.com>
2023-05-11 17:03:59 +0900

    :art: [FE] styled-components 세분화
    -
    
    closes #S08P31A301-513
    
    
    Former-commit-id: f0fc37a86a2fdf9ade34f2aaa971715b67b67600
    Former-commit-id: 8afcd40fb3d8f60cd6ec358d084d37a2f3a054c4

--- frontend/src/components/TransferPage/TabMenuComponents.tsx
+++ frontend/src/components/TransferPage/TabMenuComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:438a550cdce306e01c896b9c0d63b2b4eb95cec0cadf092028f21d480c2445eb
-size 795
+oid sha256:a23414f57397c4dc8d726b90d951b380111318899409cf7f04a6bcf35765791c
+size 806
