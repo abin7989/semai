56296094460ececa4c32627230cd088e8d9691ab
shpark0913 <shpark097754@gmail.com>
2023-05-12 09:51:10 +0900

    :lipstick: [FE] 설정 드롭다운 UI 변경
    closes #S08P31A301-510
    
    
    Former-commit-id: 91d97c020723513a9162d6dc510d98794eb63fca
    Former-commit-id: cf7a93b28914ad444e5cd5b811b53e79a6cb510b

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b3586ea7a9563e9709bcd3438cdd8c17bf0bf6cc83f9ecb4e3a896e1d9d32c5
-size 6221
+oid sha256:1c69eca68b2c3405a903da1caf70ee21cf02732613d94aefc2409d301ebb19c8
+size 6123
