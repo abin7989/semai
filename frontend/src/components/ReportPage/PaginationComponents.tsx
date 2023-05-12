c4570acfbb1820bbcff2459b87a561efbd1210e1
gyur1kim <qri1114@gmail.com>
2023-05-12 14:31:04 +0900

    :sparkles: [FE] pagination에 화살표 구현
    - 10개씩 기준으로 나눔
    
    closes #S08P31A301-497
    
    
    Former-commit-id: a32b3f3b50e32035a937ec3a29ea76d76c8f6fd6
    Former-commit-id: fcefcb45d4ce9732f179ddb177e74e690d60ed83

--- frontend/src/components/ReportPage/PaginationComponents.tsx
+++ frontend/src/components/ReportPage/PaginationComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7e4f99cb47326a8ac402216f284b7181a3e5a90a26883b3c01f1dcf3d4ce87e
-size 1741
+oid sha256:8073e870d2158e8e99fd8bafa151d98660e657185a41638099a153c098843d9c
+size 2897
