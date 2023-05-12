31e204890413e61cc88705c4920a19adb39dce70
gyur1kim <qri1114@gmail.com>
2023-05-12 13:09:38 +0900

    :recycle: [FE] selected useState로 관리하기
    - selected를 2중 배열로 관리
    - selected된 값은 checked
    
    closes #S08P31A301-513
    
    
    Former-commit-id: bae54434f2b3226b383555ec33c0f7268cc13be9
    Former-commit-id: b13899401e6f76af0562ac7576cd494ccbc626bc

--- frontend/src/components/TransferPage/TabContent.tsx
+++ frontend/src/components/TransferPage/TabContent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8010a9de0658984a75c70f394b2ebdea71cd9cbac971dbb5de84f75efd27e9e1
-size 3824
+oid sha256:257f537a8ddb09e3bdd0422a5ad8ee013466dd33d7f951ddc2d9b6b0fae50ea3
+size 7373
--- frontend/src/pages/TransferPage.tsx
+++ frontend/src/pages/TransferPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:224e83f71eaab673e946541076a49ea762c8990b0e1b9e9ffbe923c7c5bf8542
-size 1141
+oid sha256:3c5c62a89d1066add06f4741482c16b37b53f5e123a670e0140cb14bcf98e4ce
+size 1147
