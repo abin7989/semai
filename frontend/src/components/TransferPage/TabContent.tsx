e859a2525b696325b712d7338ccce9cbb7b27514
gyur1kim <qri1114@gmail.com>
2023-05-14 11:35:05 +0900

    :bug: [FE] cherry pick
    - 체리픽
    
    closes #S08P31A301-584
    
    
    Former-commit-id: 7d64c04988e6bddd9eea3d9457a7ffd1754c9526
    Former-commit-id: 420b2c18ad646348d3c418871831890e9f772de8

--- frontend/src/components/TransferPage/TabContent.tsx
+++ frontend/src/components/TransferPage/TabContent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a56175af27c2eeee9a3fdd739290d56df875e253bec80769c81ccdcb31eca2d3
-size 3931
+oid sha256:c59d322124e2314cb176dc37dab349c621de0719520c19b3f1cf9f1ed702d155
+size 3860
