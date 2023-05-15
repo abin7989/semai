fa25a1c11562f54e9dd60f465bbfed48dda98539
gyur1kim <qri1114@gmail.com>
2023-05-15 10:30:08 +0900

    :lipstick: [FE] 모달창 디자인
    - flex
    
    closes #S08P31A301-585
    
    
    Former-commit-id: 74fc82d302d9ff51dbf964aecdebd86e374a9a9c
    Former-commit-id: 4c42a9245e7a32d3c521eacf1276b2ed954feb4a

--- backend/semes/src/main/java/com/ssafy/semes/user/model/service/UserSerivce.java
+++ backend/semes/src/main/java/com/ssafy/semes/user/model/service/UserSerivce.java
@@ -1,10 +1,3 @@
-package com.ssafy.semes.user.model.service;
-
-import com.ssafy.semes.user.model.UserEntity;
-import com.ssafy.semes.user.model.UserRequestDto;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserSerivce {
-    UserEntity findUser(UserRequestDto userRequestDto) throws Exception ;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe0119c6ba99ef4619fa069da5a6104afbd3976e3c37c3343c2de3710ee092e3
+size 305
--- frontend/src/components/TransferPage/ConfirmModal.tsx
+++ frontend/src/components/TransferPage/ConfirmModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5860754675767a56b00b72161a0d9fdb01b0c094e14d7604a1ddf3b59e85a9e0
-size 2126
+oid sha256:0cccd47eeb1e444e75538694d83b2f316c1419991bffc5a34f7a1f7c48761fd9
+size 4228
