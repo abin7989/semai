450b40b3fb206e6e95cc6708b251920b86c4adb7
김영식 <kimyoungsik7@naver.com>
2023-05-15 16:22:04 +0900

    :recycles: 디텍팅 박스 테두리 증가
    closes #S08P31A301-646
    
    
    Former-commit-id: 0702c67b8a79c43eba69c80709580f6467110eee
    Former-commit-id: 54b7757020c6128398efd3b258a388f3bb8db387

--- ai/api/classification/RegNet.py
+++ ai/api/classification/RegNet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1de8caa225a282b57276c2c99791bff30412eafdcdb7afe6509b138234bb541e
-size 6726
+oid sha256:4a576c7308c7f724e04830b5637848c3552538eba43d2686ee1b0d839e4b69a7
+size 6791
