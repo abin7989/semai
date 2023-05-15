4ef0705532aff25be825e1a183e4584338dda28a
shpark0913 <shpark097754@gmail.com>
2023-05-15 13:44:49 +0900

    :lipstick: [FE] 추이 페이지 css
    - 제목 컴포넌트 생성
    
    closes #S08P31A301-600
    
    
    Former-commit-id: 61833d32aed269930c4e4759f1e55ee36b359601
    Former-commit-id: 9e3cf2f52c30040035e9b5650311ee14de0a6b2f

--- frontend/src/pages/PredictPage.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1236b034893ff879078ff91ff23043d5adeab7f26decfd18e6700b97d49a3f9e
-size 1214
---
+++ frontend/src/pages/PredictPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1cb450a4701f8f917a0864f5e3727e416b9217fd27b2a108f20f1968c2f33419
+size 2139
