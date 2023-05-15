3bf56cc9aea17b8323f77e77c51ad9304fd00ad8
abin7989 <abin7989@gmail.com>
2023-05-15 16:35:55 +0900

    :bug: [BE] 유저 및 대시보드 주석 수정 S08P31A301-652
    Former-commit-id: 3230d291a97d9f9ccc1813f15a93c722ebda29e4
    Former-commit-id: 831ab83b3aabd799292985059d2557d5453ee06e

--- backend/semes/src/main/java/com/ssafy/semes/dashboard/model/service/DashboardServiceImpl.java
+++ backend/semes/src/main/java/com/ssafy/semes/dashboard/model/service/DashboardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce6ae4c30a864c4f3d99fdaeec965debd430a73b6ddbf06f01756b01f0b6d123
-size 3928
+oid sha256:3790cdc2f3c30eeaeb0054cb537b5e36339d48019545c69b0540924a89635d9a
+size 4112
