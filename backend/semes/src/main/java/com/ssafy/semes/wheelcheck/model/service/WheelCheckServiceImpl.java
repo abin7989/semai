a9e590cc5809fdf34ea65c504bfabbdc09b0f844
abin7989 <abin7989@gmail.com>
2023-05-15 17:47:09 +0900

    :bug: [BE] ai 폰트 docker file S08P31A301-652
    Former-commit-id: a8959db6dd50372446b41160c01c5eb31119b96f
    Former-commit-id: 22133806c04b183952bbbff28669056c71cac596

--- backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/service/WheelCheckServiceImpl.java
+++ backend/semes/src/main/java/com/ssafy/semes/wheelcheck/model/service/WheelCheckServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b362d628ecdcc684195b661685fc411f38a86fc8f3202c53d75cffef1dcd53bd
-size 4022
+oid sha256:83b1ff65ed722618220f06396c07993a8e2409ff167060247d86ebcf47c32f93
+size 4021
