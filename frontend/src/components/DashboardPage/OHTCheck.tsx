ad4a16aae852db9f369f00c19e442c1aab44fe17
shpark0913 <shpark097754@gmail.com>
2023-05-15 15:19:43 +0900

    :art: [FE] NavBar 이름 변경
    - 추이 -> AI 분석
    
    closes #S08P31A301-600
    
    
    Former-commit-id: 13ffa98202a134657c5b7b49871709bfc91403f2
    Former-commit-id: b1e45471b4e639a06cb4c5654f7c88e27a4c8534

--- frontend/src/components/DashboardPage/OHTCheck.tsx
+++ frontend/src/components/DashboardPage/OHTCheck.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a11389476a81866eba6c793011ed502dc53725b5d65cdc46df9b5a0754364ae
-size 3314
+oid sha256:87a89fd14a9da047d8e6f7470876cb8e167d96a30deb50ee190bb8726a4a449e
+size 3367
