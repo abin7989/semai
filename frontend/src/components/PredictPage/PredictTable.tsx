7e857e8daaa545a3ac700a93b247c0e5663c0b17
gyur1kim <qri1114@gmail.com>
2023-05-16 23:56:48 +0900

    :lipstick: [FE] AI 분석페이지
    - title css 수정
    
    closes #S08P31A301-588
    
    
    Former-commit-id: bd322ae2d7e6c44132a5bfad31205f946e7ce3c4
    Former-commit-id: 08c2c197a97e3f7b1c8f8319a175f2d10230bb0a

--- frontend/src/components/PredictPage/PredictTable.tsx
+++ frontend/src/components/PredictPage/PredictTable.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acbc7a67d24e8bf6674489bf960fa7abcb220182709558b1d4f1a4411937dd52
-size 1596
+oid sha256:52ecc59da852baf56a897ae8f8d80df7ef5896823796c2d2aa00bde664eba33e
+size 1628
--- frontend/src/pages/PredictPage.tsx
+++ frontend/src/pages/PredictPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eb429612e0d93a2c8d7f880dbd9e975ba563f2d3d0d6978202a9ebcaf271767
-size 3430
+oid sha256:fab21302dfd162fa6a84290719ad25b44f256563cc95f87118e5f323f939a3cd
+size 3558
