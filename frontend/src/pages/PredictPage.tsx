7f9f53750cd7195be379b5639b06ac07a2901ee2
shpark0913 <shpark097754@gmail.com>
2023-05-16 13:45:35 +0900

    :sparkles: [FE] AI 분석 페이지 좌측 표 구
    closes #S08P31A301-602
    
    
    Former-commit-id: 1f4ec03eea90ac29cdc5efa6fb5f3b86947d540a
    Former-commit-id: d41ce5f5101eaad544e4c5bb3c9bc066b5427fec

--- frontend/src/pages/PredictPage.tsx
+++ frontend/src/pages/PredictPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:491bcee53cecac43d1950afe63a0b0edb675a08a6ca0cc5d9dd81a30ee085853
-size 2150
+oid sha256:c5bc4304b5d4105400504e7503ef0413c156545317c1a21603258b47b90769d7
+size 3386
