6ff6be8cfe652b956af1b6ae7efafc61e10fe6da
gyur1kim <qri1114@gmail.com>
2023-05-16 10:32:49 +0900

    :recycle: [FE] report 상태를 redux에서 관리
    - 586
    
    closes #S08P31A301-586
    
    
    Former-commit-id: e59bf24e9810a2b5283a8ed0c6cdf94dfa808851
    Former-commit-id: 41f23e030a57b0af83154c46ad4dce5f08a95d9e

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1870a70b47ef429f1298a2f55810f891057d25ba3ed6f2102b793cf2c4e7bbb6
-size 7973
+oid sha256:6659610cd31ac078c6f20a50d3222e221748a0a3bf51cbac2d43a7334105428a
+size 8411
