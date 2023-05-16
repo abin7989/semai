0e36159a64312efda5c5011861893eaf1c3af1cc
안태윤 <cfdw011@gmail.com>
2023-05-16 15:50:00 +0900

    :bug: [BE] 전이학습 데이터 API 오류 수정
    학습용 데이터 삭제시 오류 수정
    
    Closes #S08P31A301-606
    
    
    Former-commit-id: b839974cc9d0fba8688f9e8e8119e77845e46310
    Former-commit-id: fa92da22b180d5e28d99d35ae40be060aa9b4ada

--- backend/semes/src/main/java/com/ssafy/semes/util/FileUtil.java
+++ backend/semes/src/main/java/com/ssafy/semes/util/FileUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe8b2ef30e3987a221cce81e2a81a2f0be6d05987f789bafef98d27e1346e2bc
-size 2646
+oid sha256:4238615eda6fa9f6ce9b3d582930eb40e5a05fa0f95769f016a8adbd10eba4a8
+size 2693
