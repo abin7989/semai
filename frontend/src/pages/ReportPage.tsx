b28b5ea0fee062d0d173fae575cdef19dc9519a0
gyur1kim <qri1114@gmail.com>
2023-05-16 11:38:43 +0900

    :recycle: [FE] state 관리
    - page 관리
    
    closes #S08P31A301-586
    
    
    Former-commit-id: a01ac3e2efa04c01ade721f277632764fc98dcdd
    Former-commit-id: fc1ecf472321d6b058befdcce160e0f02a0ae915

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34a1415cbbd7fea2173f8a52b521b89bb2fb2b78e2a37d35f44535c39d691679
-size 8166
+oid sha256:68d2c23ffa152f91592e956db01fc8a17305a601be6e771d5f6caf9cd086a57c
+size 8231
