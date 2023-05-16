ec68cdf9fd5d1d055b74dbf31f43203e4130e7b1
abin7989 <abin7989@gmail.com>
2023-05-16 10:28:11 +0900

    [BE] 레포트 주석 추가 S08P31A301-652
    Former-commit-id: a79ff68fa930a9f8c2b68a0f8307b58fb4cd729c
    Former-commit-id: ae3d6d7f194c7020d3401e840d9dcee86e635e25

--- backend/semes/src/main/java/com/ssafy/semes/report/controller/ReportController.java
+++ backend/semes/src/main/java/com/ssafy/semes/report/controller/ReportController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a0738de7fdc88831e279382fd8a921cbedec5e3d39f47075ee6e6e7cec99603
-size 11198
+oid sha256:289747635ba8a6667b922eaa38c7d20592e9eaaf21efa002cbd27928f7a6b8e2
+size 12090
--- backend/semes/src/main/java/com/ssafy/semes/report/model/service/ReportService.java
+++ backend/semes/src/main/java/com/ssafy/semes/report/model/service/ReportService.java
@@ -1,14 +1,3 @@
-package com.ssafy.semes.report.model.service;
-
-import com.ssafy.semes.report.model.QuestionDto;
-import com.ssafy.semes.report.model.ReportListResponseDto;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
-public interface ReportService {
-    Map<String, Object> findReport(QuestionDto dto) throws Exception;
-    ReportListResponseDto findReportDetail(long wheelChcekId) throws Exception;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a47508c97bbf46fc6d33350980f4313893b5d01626eacc2b2aacecdacbc6956
+size 721
