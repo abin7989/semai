486aed847c8c1c3c974bf985f72872456a402c9e
shpark0913 <shpark097754@gmail.com>
2023-05-17 10:14:39 +0900

    :bug: [FE] 대시보드 점검 현황 자동 업데이트되는 버그 해결
    closes #S08P31A301-626
    
    
    Former-commit-id: 666bce3ac80a447600d688151cd705e7a1559930
    Former-commit-id: 4cc199503d3199a6acbc45e7eab5b3c4da9772e7

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1f89b7a55a17c3e17e15267ca14ff1cd143fc817fb5aca4092cea1578ed57a7
-size 3376
+oid sha256:e35f5b988ee519e0bf306453cf4563fde2b536fea6a31b4d6ead56ed3377caff
+size 3728
