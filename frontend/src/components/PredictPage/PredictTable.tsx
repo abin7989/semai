208ee74e1d525a8c4bb810e430af15ba12150951
shpark0913 <shpark097754@gmail.com>
2023-05-17 17:12:34 +0900

    :sparkles: [FE] AI 분석 페이지 모달창 구현
    closes #S08P31A301-625
    
    
    Former-commit-id: 5d2d24cd0c52d1ddaba8ac00ff6aa81474c7fbb6
    Former-commit-id: f45f63ec7e70c4d3f577f697decfae3b2c41979d

--- frontend/src/components/PredictPage/PredictTable.tsx
+++ frontend/src/components/PredictPage/PredictTable.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5bfaee0de09814ec5fa0eed1b6af2c5bc0c1b0e46dc3edd0c6ea10be40a9614
-size 2162
+oid sha256:95a72a3e576374a9d08580728c69146ee0dfb38784615674e300c7aef910d6f9
+size 3776
--- frontend/src/pages/PredictPage.tsx
+++ frontend/src/pages/PredictPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f85e9689fba9efb966bec743a4d5a6c86a0cbdfc0fc360f5eed423b3d97d8f9e
-size 3578
+oid sha256:bb5954f6d948b3534a9bd3dee69d976c2882d420361268786a40c102ba6c5506
+size 3637
