03b20ac1174096cc0243f5df4903fd6382104f28
YU DAVID <loveneverfails95@naver.com>
2023-05-17 17:47:18 +0900

    :sparkles: [BE] 이상탐지 모델 수정
    Former-commit-id: d0e6380cebf481a65e62ded07fd1b21e7dd80ac7
    Former-commit-id: 5dc83bd121571ce8e247d0d71ed4285551db039c

--- ai/api/detection/yolo.py
+++ ai/api/detection/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c72624daee2ef031c982014634cce2cb3315d363d30436f79f4ad567b8c97aa6
-size 3221
+oid sha256:248d7a09b0cb01ff5ccdd7a452ff520273f0dbf157c3f50fb920010943939121
+size 3222
