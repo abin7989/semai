8b7d0563d04758d5650b4c95c73a358617cf2f85
안태윤 <cfdw011@gmail.com>
2023-05-17 10:35:04 +0900

    :bug: [BE] 전이학습 시작 API 오류 수정 및 파라미터 추가
    전이학습 시작 시 하이퍼 파라미터 설정 추가
    learning rate, momentum, batch, epoch 설정
    
    Closes #S08P31A301-606
    
    
    Former-commit-id: e6c0a525764d187e369170daa75821ebb0f48b36
    Former-commit-id: d6fd5601771c97669860a603780ce1c9ad4f0da2

--- backend/semes/src/main/java/com/ssafy/semes/transition/model/service/TransitionServiceImpl.java
+++ backend/semes/src/main/java/com/ssafy/semes/transition/model/service/TransitionServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da47e5404fc6d40cca9cf00e30ad6dbf6ed9b031c9c82bee0de61c323661d089
-size 5149
+oid sha256:5f7cf397099e24e06b5bd0e009a7291807b6e9581a4304a8bfeb3ac0d287b232
+size 5606
