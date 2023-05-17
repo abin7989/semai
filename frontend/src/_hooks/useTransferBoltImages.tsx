aa1e160e29b36048e3bea15834b1306063d7b7e3
gyur1kim <qri1114@gmail.com>
2023-05-17 23:21:55 +0900

    :sparkles: [FE] 전이학습 시작 시 params 추가
    - 4가지 값을 넣음
    - form 태그 이용
    - e.preventDefault를 하고 form의 값을 Axios params로 제출
    
    closes #S08P31A301-597
    
    
    Former-commit-id: 742e836e04624b1c7ddf1120f0df0935d1864fa4
    Former-commit-id: 56a8086f05cf7ac7c9d933b4d4514c13b0f2a32a

--- frontend/src/_hooks/useTransferBoltImages.tsx
+++ frontend/src/_hooks/useTransferBoltImages.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ee4130cf6d948d02ba8ab01fcfb1b0dcd4c877920b55882c5649d072462bfed
-size 1482
+oid sha256:c22180da91727d9101dbad901b395f538cd79e829348488fa460f8693c75bca3
+size 2593
