26716633741b203fdc131967cd415f871cc496d1
gyur1kim <qri1114@gmail.com>
2023-05-17 15:26:39 +0900

    :bug: [FE] 전체 선택 checked 버그 해결
    - checked에 조건을 추가
    
    closes #S08P31A301-598
    
    
    Former-commit-id: bc156cc692aca8620b81c80528ef5869390d2c74
    Former-commit-id: 07102170241b18ae76c1bbadea61f23ef65e40a4

--- frontend/src/components/TransferPage/TabContentInfo.tsx
+++ frontend/src/components/TransferPage/TabContentInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a828fe77fedbf5ce722d08279bdb93086c4be798a371fecfba87be08f4b22da1
-size 2214
+oid sha256:4c92380feacdd674a1c496f2a13111f497f91d02c86257d75cc5d34ac336ab70
+size 2281
