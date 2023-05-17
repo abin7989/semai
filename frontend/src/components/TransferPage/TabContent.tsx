ddf1363ca11b30726d8fe1bc1c5d00efb8e735d4
gyur1kim <qri1114@gmail.com>
2023-05-17 17:40:35 +0900

    :sparkles: [FE] 학습용 이미지 삭제
    -
    
    closes #S08P31A301-598
    
    
    Former-commit-id: d880f6458be0292088c532914e77741531b4670c
    Former-commit-id: 39321b8e57d1d256d1779ed3a520a58c377c63a7

--- frontend/src/components/TransferPage/TabContent.tsx
+++ frontend/src/components/TransferPage/TabContent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f2c115543c0ccee8eea143089b03cfab468fd8ddde155a81f7c8e20df2be8b7
-size 1500
+oid sha256:d0eaaa7651b49957fa986d91fb9e8d3faebc290aa360b9ae7d0f67071a030cbf
+size 1286
