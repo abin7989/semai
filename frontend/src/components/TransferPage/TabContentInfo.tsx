2598886974ae3df37d32331db7224aa04a21fcbc
gyur1kim <qri1114@gmail.com>
2023-05-17 15:56:12 +0900

    :bug: [FE] slice 수정
    - state 변경을 엉뚱한 곳을 하고 있었음
    
    closes #S08P31A301-598
    
    
    Former-commit-id: ad14fb245a913a655171f578223af351c5dc610b
    Former-commit-id: 9367cb46d56f7fc8d89f040b0830d5c62ff9e377

--- frontend/src/components/TransferPage/TabContentInfo.tsx
+++ frontend/src/components/TransferPage/TabContentInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c92380feacdd674a1c496f2a13111f497f91d02c86257d75cc5d34ac336ab70
-size 2281
+oid sha256:21819f7db9cfc6dbf40d1f9720f8edbbab5073a0c383d92bac11e98f1231ba0c
+size 2409
