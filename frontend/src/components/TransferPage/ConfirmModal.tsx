340798419baa9d8c3773ea167b93c4d290f86110
gyur1kim <qri1114@gmail.com>
2023-05-17 14:45:41 +0900

    :sparkles: [FE] 이미지 전체선택 기능
    - 전체선택 기능 구현
    
    closes #S08P31A301-596
    
    
    Former-commit-id: c65cced6a413c0073a5947c2f794de5fa0a4ef41
    Former-commit-id: 1de82b7d60a8fefba3d85ea8d22a40e3119809ab

--- frontend/src/components/TransferPage/ConfirmModal.tsx
+++ frontend/src/components/TransferPage/ConfirmModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dcd19aae3a30a091bd5a7bd76538b5366100ebbade7b657ff0ac7906b1f8698
-size 4321
+oid sha256:055ca87bfed8b5391852d8481b757df4a26fc294cbdf9ed665e344daa198a9d5
+size 4333
--- frontend/src/components/TransferPage/TabContentInfo.tsx
+++ frontend/src/components/TransferPage/TabContentInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:925ab78e8f687a03cf9a48f6a7613fd1eaf733da9b41282d1debe52a5497b085
-size 935
+oid sha256:a828fe77fedbf5ce722d08279bdb93086c4be798a371fecfba87be08f4b22da1
+size 2214
