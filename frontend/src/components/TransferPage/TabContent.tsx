f8242f904b352350f38594552a344cd2a3de67ff
gyur1kim <qri1114@gmail.com>
2023-05-17 14:45:41 +0900

    :sparkles: [FE] 이미지 전체선택 기능
    - 전체선택 기능 구현
    
    closes #S08P31A301-596
    
    
    Former-commit-id: c65cced6a413c0073a5947c2f794de5fa0a4ef41
    Former-commit-id: 3091f35c42ee7e11b9a43f6680a9bef96929f46b

--- frontend/src/components/TransferPage/TabContent.tsx
+++ frontend/src/components/TransferPage/TabContent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea29e9ac9064f59229861fa1ed6ea0d05dd0c1ca2ec9445a704af76e19b3bc4c
-size 1522
+oid sha256:6a6d5588324da68a08d91a6a565c3521715104d1895740d4140ce7628c3c8c69
+size 1554
--- frontend/src/components/TransferPage/TransferButtons.tsx
+++ frontend/src/components/TransferPage/TransferButtons.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68b134249200c10523225428bf054275cb5fac186f7a06fbd463200c24b52caf
-size 4436
+oid sha256:3283eb272fdb0af8267424a1595cec8155828bfa5bcee0d103dd665455c59232
+size 4434
