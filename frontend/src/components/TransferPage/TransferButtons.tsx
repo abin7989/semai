6ee2eb223567b816cd1e93bde09734815b79ec2b
gyur1kim <qri1114@gmail.com>
2023-05-17 12:46:21 +0900

    :sparkles: [FE] 학습하기 버튼
    - axios.get을 이용해 요청 보내는 버튼
    - 아직 서버 에러남
    
    closes #S08P31A301-596
    
    
    Former-commit-id: 73f630f2514e33de303d4fa055ddacf916da8bca
    Former-commit-id: 8bd567efeae21a38c11ac8678e3366d036ce9c21

--- frontend/src/components/TransferPage/TransferButtons.tsx
+++ frontend/src/components/TransferPage/TransferButtons.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d12d27abeaf7d733cce63688b800b78e7b8aec4850e7439d385b6178842c09d5
-size 4471
+oid sha256:68b134249200c10523225428bf054275cb5fac186f7a06fbd463200c24b52caf
+size 4436
