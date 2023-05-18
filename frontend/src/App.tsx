72478b65d99d35bf00232084b949cc9fcfa3baea
gyur1kim <qri1114@gmail.com>
2023-05-18 16:06:22 +0900

    :sparkles: [FE] toastify 알람 생성
    - css를 적용하지 않으면 toastify가 적용되지 않는다.
    
    closes #S08P31A301-605
    
    
    Former-commit-id: a341b57f1ad802dabf60fd472947a8f9366e3b28
    Former-commit-id: a56af8d1fc9424d2628eae48ce0fb781ba28354e

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20ca0cfa92871dec6b88b6d493c75cb016e20c285beb9e110f49d586cc8700f2
-size 1623
+oid sha256:6a6e00796e934cb22f2e6e02ce5cc8e7c4727c1005b8762084e2b8b11c9c3d74
+size 1991
