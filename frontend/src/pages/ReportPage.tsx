60f8184d5efa55691c2d4a40059d2d9559879c42
gyur1kim <qri1114@gmail.com>
2023-05-18 10:31:39 +0900

    :lipstick: [FE] 조회 버튼 위치 변경
    - 기존 : 당일 , 7일, 30일
    - 30일 , 7일, 당일로 변경
    
    closes #S08P31A301-609
    
    
    Former-commit-id: 46ec4835d4aabe2a2d30a9a7d050559e5b4a21fc
    Former-commit-id: 71957c4a34118808246896b3645be362f1c8f311

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a25619ad5da7109fd58f25d3283e290fec39ee3aa0b15a063c6d8f0bb3cc6aa8
-size 8153
+oid sha256:e412fa190f7c4c40a3fa5aed7edf273953cfc72f261ba12c71b67a3f56820ecb
+size 8146
