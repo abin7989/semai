49795f8316823b45603b04584510d1dadff0a053
abin7989 <abin7989@gmail.com>
2023-05-18 15:56:13 +0900

    [BE] 예측 모델 추가 S08P31A301-652
    Former-commit-id: d133f999254393f492b656f12342dd25092b3b8c
    Former-commit-id: 81e2cf8fbd6ad32864a43b67ee76a097e61d47b6

--- backend/semes/src/main/java/com/ssafy/semes/report/controller/ReportController.java
+++ backend/semes/src/main/java/com/ssafy/semes/report/controller/ReportController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d070c2466e6091e2dbdb907d618ceaa95b732420080e35fc1e74176f2c4bb2c
-size 14598
+oid sha256:3478d8fc0572687b1bd0a35787d18b5673050ee456a19d5db2d6826e83711dea
+size 14259
--- backend/semes/src/main/java/com/ssafy/semes/report/model/service/ReportService.java
+++ backend/semes/src/main/java/com/ssafy/semes/report/model/service/ReportService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80bbbe3123f532ed8e10e58befae8ff6d7e070686dfe52469501d7793b36abe3
-size 919
+oid sha256:2cbdf5d406e30df9d08b4d53faa97181543ce227494b3b01aaaed6426a51ae75
+size 751
