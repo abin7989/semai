041b6861dbeb54d2f028104cda19284e595ff676
gyur1kim <qri1114@gmail.com>
2023-05-18 10:29:54 +0900

    :bug: [FE] input의 defaultValue가 변하지 않는 문제 해결
    - useEffect를 이용해 query가 변하면 redux의 queryObj 변경
    
    closes #S08P31A301-609
    
    
    Former-commit-id: 5cc3cfbc8743f4a863bcf9d013e1dd67eea2fc45
    Former-commit-id: 81e647b353273c85e270a097116f99caa29a81fd

--- frontend/src/_store/slices/reportPageSlice.ts
+++ frontend/src/_store/slices/reportPageSlice.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e68ffd6cf29f4f7e3baf574c1e37290e4a39a1689b62ab736057c0f83113bb0
-size 733
+oid sha256:e5d580825772b6c9a092cc0512cfce98d83f2ec80e1a05489a70c5163783341b
+size 669
