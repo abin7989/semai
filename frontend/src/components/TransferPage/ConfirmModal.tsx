70856f0bb86f3451536d7710481a587a43ab879c
gyur1kim <qri1114@gmail.com>
2023-05-18 10:56:40 +0900

    :bug: [FE] key props 추가
    - key props 추가
    
    closes #S08P31A301-605
    
    
    Former-commit-id: c684a15bf4c9ce570b7c865de57a3272735c05ab
    Former-commit-id: 63fef67403d6277c0a898df747829a7c4c0ffb37

--- frontend/src/components/TransferPage/ConfirmModal.tsx
+++ frontend/src/components/TransferPage/ConfirmModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7524e1530b7afe51ca777137b1f8bed53e2386d69ebdfb7cd9028048285b45e
-size 5254
+oid sha256:864c2b3760c97d87bbf099e8152e66d3adbca81628061903f960a810aca59990
+size 5494
