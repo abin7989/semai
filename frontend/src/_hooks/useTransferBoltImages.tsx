9f1a5cb4589a7b2edec2b05ae83f67e7bc15fcff
gyur1kim <qri1114@gmail.com>
2023-05-18 16:06:22 +0900

    :sparkles: [FE] toastify 알람 생성
    - css를 적용하지 않으면 toastify가 적용되지 않는다.
    
    closes #S08P31A301-605
    
    
    Former-commit-id: a341b57f1ad802dabf60fd472947a8f9366e3b28
    Former-commit-id: ddb9cd4b22c73967b8f782ac7c4ae0a950cd99b6

--- frontend/src/_hooks/useTransferBoltImages.tsx
+++ frontend/src/_hooks/useTransferBoltImages.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c22180da91727d9101dbad901b395f538cd79e829348488fa460f8693c75bca3
-size 2593
+oid sha256:6baf9591be3885f785fbff2ac640615cfb654dff481310c846608aab417763f7
+size 3015
