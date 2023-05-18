4c978ad783d5356bfaf4ae3d3c02cbdae82d75a9
YU DAVID <loveneverfails95@naver.com>
2023-05-18 09:13:17 +0900

    :bug: [BE] 이상탐지 폴더 path 추가
    Former-commit-id: a007616846af8950067f178334ce64db05de9e48
    Former-commit-id: 6d70784ba717f157355ae88356dbae36b60c7a2c

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02c497ca264c6546e5e479684f00c99e0dbe220cd3f30c395cab4260f1aa0b02
-size 6004
+oid sha256:eef415a12651edbaed8fb2183b6782108cf9388ddf4882e69b383d0fd936ba32
+size 6056
