59e049c23654e5a86b6e5ed74824439cff8828a6
gyur1kim <qri1114@gmail.com>
2023-05-18 16:45:49 +0900

    :sparkles: [FE] 전이학습 완료 모달창
    - 전이학습 진행 중 loading
    
    closes #S08P31A301-605
    
    
    Former-commit-id: 3bd81e6e1e1ed7016a492c855ffb5d68cfc62c4c
    Former-commit-id: fbe97cef2e8614d2743ac5fbe7ff3fa3d2ad89ef

--- frontend/src/_hooks/useTransferBoltImages.tsx
+++ frontend/src/_hooks/useTransferBoltImages.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6baf9591be3885f785fbff2ac640615cfb654dff481310c846608aab417763f7
-size 3015
+oid sha256:1b519225bc71cd6851d1132c9e6877ddc4098dbefcce5360f4823dc059ded5d0
+size 3188
