a82acded449c488d027693fec05dac61938db5b3
gyur1kim <qri1114@gmail.com>
2023-05-18 13:43:50 +0900

    :lipstick: [FE] 전이학습 파라미터 설정 css
    - 새로고침 버튼
    
    closes #S08P31A301-605
    
    
    Former-commit-id: 213ee0fc4e2fa061bf6c3695d9e5d6477419970a
    Former-commit-id: 248d38d4c681d1954d7d67acb2df2ac00de0d4da

--- frontend/src/components/TransferPage/ConfirmModal.tsx
+++ frontend/src/components/TransferPage/ConfirmModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:864c2b3760c97d87bbf099e8152e66d3adbca81628061903f960a810aca59990
-size 5494
+oid sha256:a6c43eeee845e216ca0d3d5284d6428c1af0df9549a39097ad664405009c3981
+size 5758
