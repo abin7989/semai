ab315d2eb9725542890ee7a502f561cb11cbea5c
gyur1kim <qri1114@gmail.com>
2023-05-19 10:44:09 +0900

    :lipstick: [FE] 그래프 크기 변경
    - 640px로 변경
    
    
    Former-commit-id: e377fe9bb0619d729fb251cfe96402ef52be1065
    Former-commit-id: b1d433344d0c5d5092e5c3e914a2ac97b9734296

--- frontend/src/components/PredictPage/ScatterGraph.jsx
+++ frontend/src/components/PredictPage/ScatterGraph.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50f9905cf2e894a77172ebbe05de357df1ce9cc2fdbd9e3f24be02b56ebc1fd9
+oid sha256:0f41e8fc9f9799d78a0ac72441ebb34477f54c7e50b0531fdf3911ab8ff6fc17
 size 1369
