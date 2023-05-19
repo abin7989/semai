6301a38c7a4a3407f2160d4a57eea83fa6382dbd
gyur1kim <qri1114@gmail.com>
2023-05-19 10:57:17 +0900

    :sparkles: [FE] 토큰 없으면 login으로 redirect
    Former-commit-id: c3116aff52664c689bc5565f99dcb0d522228881
    Former-commit-id: 761f8565f50784ddad4d5301688af81f803a8670

--- frontend/src/pages/LayoutPage.tsx
+++ frontend/src/pages/LayoutPage.tsx
@@ -1,25 +1,3 @@
-import NavBar from "../components/NavBar";
-import Main from "../components/MainComponent";
-import styled from "styled-components";
-import { Outlet } from "react-router-dom";
-import React from "react";
-
-const Layout = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-`;
-
-function LayoutPage() {
-  return (
-    <Layout>
-      <NavBar />
-      <Main>
-        <Outlet />
-      </Main>
-    </Layout>
-  );
-}
-
-export default LayoutPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:152ddcbaa48ccfed755a69976120986b60b7d69921f3884373e1a5d7b2a5e530
+size 726
