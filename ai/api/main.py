bc62b19ce798aed0a6c97e8635e5ed0385599635
YU DAVID <loveneverfails95@naver.com>
2023-05-22 17:56:21 +0900

    :bug: [BE] 이상탐지 모델 API 연동 마무리
    Former-commit-id: f5c03afb5b10b3104b1923aba13d23824f69dec1
    Former-commit-id: a08014e9cbf0fec91a46d05d664e1ca8e8500b18

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eef415a12651edbaed8fb2183b6782108cf9388ddf4882e69b383d0fd936ba32
-size 6056
+oid sha256:cca5323d30ae1e9fcbef38bf19349fc96086bf4c51181cb3ace75cad5bdfaf9c
+size 5636
