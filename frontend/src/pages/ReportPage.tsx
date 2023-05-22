3b84d088648380893892edcebc067a101d7cfc5d
gyur1kim <qri1114@gmail.com>
2023-05-22 12:01:25 +0900

    :recycle: [FE] pagination component 분리
    Former-commit-id: a81225fb0da282b33e0030bb2e0dc8dd52af28b0
    Former-commit-id: bd39c26a8ac3eb67acaeda2fd32c85c9c49f38e6

--- frontend/src/pages/ReportPage.tsx
+++ frontend/src/pages/ReportPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b989696bb7e4d999736da366189435725de5ecd0cef1d40dee09b19290d68b90
-size 3076
+oid sha256:b89b1648df8c6810dbf07ba90e3a1d11f3c5579e6ab1e0cbbc3739191546d470
+size 3046
