e1f3b4cb2d9f93ce5583d2d671a9bb74b4700476
gyur1kim <qri1114@gmail.com>
2023-05-23 17:18:52 +0900

    :bug: [FE] 차주 이상휠 조회 URL 수정
    - BASE_URL 수정
    
    
    Former-commit-id: 3f4806284ddb85661a0d17175b744e9e5996eb07
    Former-commit-id: e4681b3ce6fa7958141a78f14029b7f6819d94dd

--- frontend/src/pages/PredictPage.tsx
+++ frontend/src/pages/PredictPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e7aecce4f46c63c0478a5a6ed7167e4b3413133ea4432ecd6bb84c497b26412
-size 13135
+oid sha256:63fceec837d70b24ca7eea77765e27fcb08bb842d2616ec0929a56e76a5d8660
+size 13170
