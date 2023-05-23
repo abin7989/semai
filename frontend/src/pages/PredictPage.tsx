db8514d0f2cc6d87b7997e54e1022d178d3a7c1c
shpark0913 <shpark097754@gmail.com>
2023-05-23 20:31:52 +0900

    :bug: [FE] AI 분석 페이지 input 태그 자동완성 버그 수정
    Former-commit-id: 0475c2ec3aab454bb782e8c1efb3a91bd17a6c09
    Former-commit-id: 73517114994d1c4ce7a01b943d80b5dac7373289

--- frontend/src/pages/PredictPage.tsx
+++ frontend/src/pages/PredictPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63fceec837d70b24ca7eea77765e27fcb08bb842d2616ec0929a56e76a5d8660
-size 13170
+oid sha256:498aa96cd2d07f0d90977c3dada45343e6d08ccd856aaecf089c4ff21a15ead9
+size 13541
