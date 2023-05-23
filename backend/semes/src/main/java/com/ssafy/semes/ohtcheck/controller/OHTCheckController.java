da5da80e323422ac5b1706364677a11a6e17a99d
abin7989 <abin7989@gmail.com>
2023-05-23 17:05:54 +0900

    [BE] OHT Anomaly
    Former-commit-id: d872c4beaec4c34d85eae82b77757f565ac7daba
    Former-commit-id: 7b3c46e6ea257bc22d3d1d568715c4df87a5a1e7

--- backend/semes/src/main/java/com/ssafy/semes/ohtcheck/controller/OHTCheckController.java
+++ backend/semes/src/main/java/com/ssafy/semes/ohtcheck/controller/OHTCheckController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a099deb4f8893eae2f7a02c6ccbdf41318ad020f1deff57aab5777a0ba3d8e30
-size 7394
+oid sha256:1b6821095512ffb77c0edfcf6bd1b403999ea26357f76a5cf0caeb262b69f547
+size 7470
