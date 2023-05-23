f7c1a35a88a9cf74a4ca3af4b85d9b715d4fca95
shpark0913 <shpark097754@gmail.com>
2023-05-23 21:15:06 +0900

    :lipstick: [FE] AI 분석 페이지 하단 설명 수정
    Former-commit-id: 0133f83215e0622c10633a4ef84446717fed7b6f
    Former-commit-id: 619d7f847c715df4821a92a0ea75652779f7c808

--- frontend/src/pages/PredictPage.tsx
+++ frontend/src/pages/PredictPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:498aa96cd2d07f0d90977c3dada45343e6d08ccd856aaecf089c4ff21a15ead9
-size 13541
+oid sha256:854d8e601903a3ec0b0e5c954b49d647daea0e5282b9845ecdf98589c8089a18
+size 13547
