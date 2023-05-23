99d43845bc1555bc800feab8f9929f3b0ec235e8
gyur1kim <qri1114@gmail.com>
2023-05-23 13:42:57 +0900

    :sparkles: [FE] 모델 교체 알람 멘트 수정
    - 기존 모델의 정확도가 더 높아 모델이 교체되지 않았습니다.
    
    
    Former-commit-id: 1a2a9703ec13234ed09fbbddfa9eb07acea1410f
    Former-commit-id: 504ca717fce0e8a1eb7fc33f279a502561d28fda

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7ed05d75c5e2eda7bb38c300cd2bffd742d29a3db8fe2a57b230fdc22217bb7
-size 2123
+oid sha256:d16498e7661879405a018b7fc8e03e65f4332c7d7a25a2332ee4be7336c82304
+size 2071
