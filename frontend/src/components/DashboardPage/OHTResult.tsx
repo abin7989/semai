c969a7c686486c59e099091f00154720c5914831
shpark0913 <shpark097754@gmail.com>
2023-05-23 21:39:26 +0900

    :lipstick: [FE] 새로고침 버튼 css 수정
    Former-commit-id: c8f58149d09fae8009f75d791fb72ce55121b23f
    Former-commit-id: fdbb5631117518427eac294c5e0452548e7260ed

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7bbcc48f27aeabd9608c20cf705a84a3935ab95447cc408d5a3a81eb202bd79
-size 9027
+oid sha256:56d79ff3d851baa1b5287c740e24aa77f2e226a76304af7c6036589392d6115f
+size 9038
--- frontend/src/pages/PredictPage.tsx
+++ frontend/src/pages/PredictPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:854d8e601903a3ec0b0e5c954b49d647daea0e5282b9845ecdf98589c8089a18
-size 13547
+oid sha256:ddd7d1cf1de7687dbb82c782295ba4edb14a92d30b7c040f548221c66bd899e7
+size 13541
