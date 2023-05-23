413beb084864a66f3f8eb67f05ce547cad03be75
shpark0913 <shpark097754@gmail.com>
2023-05-23 19:49:41 +0900

    :lipstick: [FE] 대시보드 새로고침 버튼 inquire에 따라 편집
    - inquire가 true이면 보이고
    - inquire가 false이면 보이지 않음
    
    
    Former-commit-id: 9cbfdf558951f0d0381ac68f3802aeaadaf803ee
    Former-commit-id: 45e4abd60cf1774d62e5051a1cdbc7f256ecb0f2

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0345195279c231152c1a14b56f31e88dcb56e2507359c6da481b3bc6e984e65a
-size 8807
+oid sha256:c4eb4cfc068701d6a0ebf5194f997889e22d4489769fa2b0c03fc914113638fd
+size 8899
