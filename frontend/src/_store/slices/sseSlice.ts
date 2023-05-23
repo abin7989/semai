4485963a31e7d1cc0f673a484f219c22f89cfdf0
shpark0913 <shpark097754@gmail.com>
2023-05-23 14:33:23 +0900

    :recycle: [FE] 대시보드 코드 리팩토링
    - DashboardLoader 생성해서 리팩토링 진행 중
    
    
    Former-commit-id: e2a267f33ce3e11b79efcca31915764e845d130e
    Former-commit-id: 05e76f011931ee0a4cbda1385b8971fa9c071567

---
+++ frontend/src/_store/slices/sseSlice.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7e4b45263514e4deea47e2ef0319f023655b8d1a9b220b2d504fea8bec849dc1
+size 1900
---
+++ frontend/src/pages/DashboardLoader.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4bcf6e479c7b3a3eda01d5c4ed18a316bbf83ee22a93a45efa54fd7ff2a4552f
+size 1108
