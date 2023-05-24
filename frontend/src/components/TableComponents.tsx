c255dedf70c4ffdd0c826685979eb993d82acd5c
shpark0913 <shpark097754@gmail.com>
2023-05-24 10:00:41 +0900

    :lipstick: [FE] AI 분석 페이지 표 css 수정
    Former-commit-id: 8ade173d72839a8cf14be68f833d95aa63884293
    Former-commit-id: feb03dd3b55fcb3f0703b63ab2acdcf4e5c2b523

--- frontend/src/components/TableComponents.tsx
+++ frontend/src/components/TableComponents.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1440fd6f0264bde2f2f98d9cb489944ce1c7659547117456bdf9725c6cc3fdba
-size 2859
+oid sha256:0030af13571fc41c608f0ad6353b7ed95832525a9a6d2b0ceca5b7de4b9d1168
+size 2965
