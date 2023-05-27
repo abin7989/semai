ea7ed737299c1eff3fae4baaec942fc98ac93dad
gyur1kim <qri1114@gmail.com>
2023-05-27 18:07:47 +0900

    :lipstick: [FE] dashboard에서 유실, 파단, 풀림은 빨간 색으로 표기
    Former-commit-id: a8ec44e005c03f7194ad690279ea657a792ff26c
    Former-commit-id: 30505535f7623b785a82d8b0e14edec450b4f568

--- frontend/src/components/DashboardPage/OHTResult.tsx
+++ frontend/src/components/DashboardPage/OHTResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f761f82a6eec9b39d0929188fd0713c1a6e85e3e904f1ea6e9a7cd6128046a0b
-size 8853
+oid sha256:a151d27e07935817768f87deb99bbb9e161900b4527badec7f9c37c16e804a29
+size 9193
