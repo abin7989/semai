5e448d289a6b94d342445a3a41f5c6479a176a46
YU DAVID <yudavid0611@gmail.com>
2023-06-07 11:55:20 +0900

    :memo: [docs] README 수정 및 이미지 삭제
    Former-commit-id: 51357e354e2bcd5a2dcfdeab23ba1904d8b57272
    Former-commit-id: 0dcd7ae54c938c02eaac2774988c9437a93ad3ac

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a070a83d47a894d2ae694c72053fd26e5c00998fcb539613ba0d5fc844e7b7e4
-size 17828
+oid sha256:5e5a6daf99de5a9ec8ec09bfd5b8ce81ab52bcafd35727da69272b2ee136c712
+size 17780
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:131ef429ead2667e8dbb0e80fa2b07e4220caf395567fb9b5682c9794ebf804f
-size 162
+import Login from "../components/LoginPage/Login";
+
+function LoginPage() {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+}
+export default LoginPage;
